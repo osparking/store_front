ad70f332371b332b4c2ba90c6e4e215e776af035
osparking <jbpark03@gmail.com>
2025-03-04 06:53:09 +0900

    insert Home after div in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Home from "./component/home/Home";
 
 function App() {
   return (
-    <div className="text-primary">범이비누가 환영합니다.</div>
+    <main className="">
+      <div className="text-primary">범이비누가 환영합니다.</div>
+      <Home/>
+    </main>
   );
 }
 
