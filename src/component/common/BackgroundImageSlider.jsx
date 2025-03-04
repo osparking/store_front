e8570e795d929f07bbe7e51ec001347a5a24b364
osparking <jbpark03@gmail.com>
2025-03-04 22:04:31 +0900

    background image slider compo - 1st cut

---
+++ src/component/common/BackgroundImageSlider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import bgo1 from "../../assets/images/bg/bgo1.jpg"
+import bgo2 from "../../assets/images/bg/bgo2.jpg"
+import bgo3 from "../../assets/images/bg/bgo3.jpg"
+import bgo4 from "../../assets/images/bg/bgo4.jpg"
+const BackgroundImageSlider = () => {
+  const images = [bgo1, bgo2, bgo3, bgo4];
+  const [index, setIndex] = useState(0);
+  return <div></div>;
+};
+
+export default BackgroundImageSlider
