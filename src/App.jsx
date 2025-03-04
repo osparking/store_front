af3cb06b6770c10e213ae08db68104a27154558e
osparking <jbpark03@gmail.com>
2025-03-04 22:23:53 +0900

    use Background image slider in App compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./component/home/Home";
+import BackgroundImageSlider from "./component/common/BackgroundImageSlider";
 
 function App() {
   return (
     <main className="">
-      <h4 className="text-primary text-center mt-5">범이비누가 환영합니다.</h4>
-      <Home />
+      <BackgroundImageSlider>
+        <h4 className="text-primary text-center mt-5">
+          범이비누가 환영합니다.
+        </h4>
+        <Home />
+      </BackgroundImageSlider>
     </main>
   );
 }
