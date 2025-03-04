3aeb0a4b6cc21c0f8f09c848ee9a1a59cd5f2dc1
osparking <jbpark03@gmail.com>
2025-03-05 05:17:21 +0900

    router w/ one element having nested element

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./component/home/Home";
 import BackgroundImageSlider from "./component/common/BackgroundImageSlider";
+import { Route } from "react-router-dom";
+import RootLayout from "./component/layout/RootLayout";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+
   return (
     <main className="">
       <BackgroundImageSlider>
