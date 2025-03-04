dfef2f4ab67013977dba58a7a25e25063e5f52fd
osparking <jbpark03@gmail.com>
2025-03-04 22:15:23 +0900

    background image slider compo - 2nd cut

--- src/component/common/BackgroundImageSlider.jsx
+++ src/component/common/BackgroundImageSlider.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Carousel } from 'react-bootstrap'
 import bgo1 from "../../assets/images/bg/bgo1.jpg"
 import bgo2 from "../../assets/images/bg/bgo2.jpg"
 import bgo3 from "../../assets/images/bg/bgo3.jpg"
@@ -6,7 +7,21 @@ import bgo4 from "../../assets/images/bg/bgo4.jpg"
 const BackgroundImageSlider = () => {
   const images = [bgo1, bgo2, bgo3, bgo4];
   const [index, setIndex] = useState(0);
-  return <div></div>;
+  return (
+    <div className="background-slider">
+      <Carousel activeIndex={index} onSelect={handleSelect} interval={20000}>
+        {images.map((background, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={background}
+              alt={`제 ${index} 슬라이드`}
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
 };
 
 export default BackgroundImageSlider
