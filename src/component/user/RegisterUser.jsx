5bf585b967b6feaa4129f5e6d86231d2320069c9
osparking <jbpark03@gmail.com>
2025-03-05 20:11:54 +0900

    resets user register form input fields

--- src/component/user/RegisterUser.jsx
+++ src/component/user/RegisterUser.jsx
@@ -41,6 +41,18 @@ const RegisterUser = () => {
     }
   }
 
+  const handleReset = () => {
+    setUser({
+      fullName: "",
+      mbPhone: "",
+      email: "",
+      usable: "",
+      password: "",
+      userType: "",
+      dept: "",
+    });
+  }
+
   return <div>유저 등록 정보 입력 폼</div>;
 };
 
