42ea5f153113d679c7b3afa7f8c17c8fd03fdf47
osparking <jbpark03@gmail.com>
2025-03-05 11:08:48 +0900

    embed NavBar into RootLayout for '/' path

--- src/component/layout/RootLayout.jsx
+++ src/component/layout/RootLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BackgroundImageSlider from "../common/BackgroundImageSlider";
+import NavBar from "./NavBar";
 
 const RootLayout = () => {
   return (
     <main>
+      <NavBar />
       <BackgroundImageSlider />
       <div>
         <Outlet />
