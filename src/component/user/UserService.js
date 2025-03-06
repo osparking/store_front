00dee6f1ad26a0e848b7c364435d2038cadb6b4e
osparking <jbpark03@gmail.com>
2025-03-06 10:48:32 +0900

    registers user by calling post method of api

---
+++ src/component/user/UserService.js
@@ -0,0 +1,10 @@
+import api from "../util/api";
+
+export async function registerUser(user) {
+  try {
+    const result = await api.post("/user/add", user);
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
+}
