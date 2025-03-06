2eeacec03a10e7664a61368775e5995ac45382ea
osparking <jbpark03@gmail.com>
2025-03-06 11:18:32 +0900

    import registerUser function from RegisterUser compo

--- src/component/user/RegisterUser.jsx
+++ src/component/user/RegisterUser.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import BsAlertHook from '../hook/BsAlertHook';
+import { registerUser } from './UserService';
 
 const RegisterUser = () => {
   const [user, setUser] = useState({
