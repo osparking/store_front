d4415a2698d5cfd6de42b768e09f119cb09b9769
osparking <jbpark03@gmail.com>
2025-03-06 12:35:21 +0900

    route register_user to RegisterUser

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import {
 import "./App.css";
 import Home from "./component/home/Home";
 import RootLayout from "./component/layout/RootLayout";
+import RegisterUser from "./component/user/RegisterUser";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path="/register_user" element={<RegisterUser />} />
       </Route>
     )
   );
