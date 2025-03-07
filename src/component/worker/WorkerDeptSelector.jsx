4bb75264ea95e01a216711e5a2f615e5f6b5d80e
osparking <jbpark03@gmail.com>
2025-03-07 09:00:23 +0900

    worker dept selector - read all dept from DB

---
+++ src/component/worker/WorkerDeptSelector.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { getAllDept } from "./WorkerService";
+
+const WorkerDeptSelector = () => {
+  const [workerDepts, setWorkerDepts] = useState([]);
+
+  useEffect(() => {
+    readDepts = async () => {
+      try {
+        const response = await getAllDept();
+        setWorkerDepts(response.data);
+        console.log("소속 목록: ", response.data);
+      } catch (error) {
+        console.error("소속 목록 채취 오류: ", error);
+      }
+    };
+    readDepts();
+  }, []);
+  return <div></div>;
+};
+
+export default WorkerDeptSelector;
