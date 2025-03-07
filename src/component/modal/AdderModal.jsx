10c2692a9e0933a6e2595f7b27b5da6c750be6e0
osparking <jbpark03@gmail.com>
2025-03-07 16:03:19 +0900

    add body to the dept adder modal

--- src/component/modal/AdderModal.jsx
+++ src/component/modal/AdderModal.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Modal } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Modal } from "react-bootstrap";
 
 const AdderModal = ({ show, closer, label }) => {
+  const [itemValue, setItemValue] = useState("");
+  const contentChanged = (e) => {
+    setItemValue(e.target.value);
+  };
+
   return (
     <Modal show={show} onHide={closer}>
       <Modal.Header closeButton>
         <Modal.Title>새 {label} 추가</Modal.Title>
       </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-1">
+            <Form.Label>{label} 명</Form.Label>
+            <Form.Control
+              type="text"
+              value={itemValue}
+              placeholder={`(새 ${label.toLowerCase()} 입력)`}
+              onChange={contentChanged}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 };
 
-export default AdderModal
+export default AdderModal;
