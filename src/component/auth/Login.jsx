3947fc448b1f1b389a7b422bbabec885d06b022a
osparking <jbpark03@gmail.com>
2025-03-08 09:54:18 +0900

    login component return value - skeleton

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const handleChange = (e) => {
     setCreden({ ...credentials, [e.target.name]: e.target.value });
   };
-  return <div></div>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <Form></Form>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
