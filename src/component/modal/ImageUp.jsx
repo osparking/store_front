fbb756ba143f5e15f4f590e929626e318083fb88
osparking <jbpark03@gmail.com>
2025-03-08 18:28:52 +0900

    simplify ImageUp compo by removing useEffect() usage

--- src/component/modal/ImageUp.jsx
+++ src/component/modal/ImageUp.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import { getUserById } from "../user/UserService";
 
-const ImageUp = () => {
-  // 1. 직원 읽기
-  // 2. 원래 영상 소지 여부 검사
-  // - 여: 기존 영상 갱신
-  // - 부: 새로운 영상 업로드
+const ImageUp = ({ user }) => {
+  console.log("유저: ", user);
+  // * 원래 영상 소지 여부 검사
+  //   - 여: 기존 영상 갱신
+  //   - 부: 새로운 영상 업로드
 
-  const [emp, setEmp] = useState(null);
   const [file, setFile] = useState(null);
   const {
     successMsg,
@@ -19,17 +17,6 @@ const ImageUp = () => {
     alertError,
     setAlertError,
   } = BsAlertHook();
-
-  useEffect(async () => {
-    try {
-      const result = await getUserById(userId);
-      console.log("result.data : ", result.data);
-      setEmp(result.data);
-    } catch (e) {
-      setErrorMsg(e.response.data.message);
-      setAlertError(true);
-    }
-  }, [userId]);
 
   return <div>영상 올리기</div>;
 };
