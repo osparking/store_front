65fc309ae5874125f9d5c9dc9611a86e00a13d9d
osparking <jbpark03@gmail.com>
2025-03-08 18:24:06 +0900

    user dashboard passes user info to imageUp

--- src/component/user/UserProfile.jsx
+++ src/component/user/UserProfile.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import ImageUp from "../modal/ImageUp";
 
-const UserProfile = () => {
+const UserProfile = ({ user }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+      <React.Fragment>
+        <Row>
+          <Col>
+            <Card>
+              <div>
+                <ImageUp user={user} />
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </React.Fragment>
+    </Container>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
