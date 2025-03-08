b58277a93c38f76bd78770082384c4fe270e800c
osparking <jbpark03@gmail.com>
2025-03-08 14:34:11 +0900

    reads employee user calling api get

--- src/component/user/UserService.js
+++ src/component/user/UserService.js
@@ -8,3 +8,12 @@ export async function registerUser(user) {
     throw err;
   }
 }
+
+export async function getUserById(userId) {
+  try {
+    const result = await api.get(`/user/${userId}/get`);
+    return result.data;
+  } catch (err) {
+    throw err;
+  }
+}
