6300548e8b19472cf3d22dc8aaee1d5dd351e717
osparking <jbpark03@gmail.com>
2025-03-08 12:19:46 +0900

    employee face image uploader - skeleton

---
+++ src/component/modal/ImageUp.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const ImageUp = () => {
+  // 1. 직원 읽기
+  // 2. 원래 영상 소지 여부 검사
+  // - 여: 기존 영상 갱신
+  // - 부: 새로운 영상 업로드
+
+  const [emp, setEmp] = useState(null);
+  const [file, setFile] = useState(null);
+
+  return <div>영상 올리기</div>;
+};
+
+export default ImageUp;
