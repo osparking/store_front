d05789465b040eeb5fde00112b2cfc8e0d6700de
osparking <jbpark03@gmail.com>
2025-03-09 11:55:38 +0900

    employee (proof) image display compo

---
+++ src/component/common/EmpImage.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import { Card } from "react-bootstrap";
+import placeholder from "../../assets/images/placeholder.png";
+
+const EmpImage = ({ empPhoto, altText = "직원 사진" }) => {
+  return (
+    <Fragment>
+      {empPhoto ? (
+        <Card.Img
+          src={`data:image/png;base64, ${empPhoto}`}
+          className="user-image"
+          alt={altText}
+        />
+      ) : (
+        <Card.Img src={placeholder} className="user-image" alt={altText} />
+      )}
+    </Fragment>
+  );
+};
+
+export default EmpImage;
