fd58f0da5828809bf3fdbdf4b8802447317f801e
osparking <jbpark03@gmail.com>
2025-03-09 14:14:26 +0900

    photo delete link, conditional style

--- src/component/user/UserProfile.jsx
+++ src/component/user/UserProfile.jsx
@@ -4,7 +4,7 @@ import EmpImage from "../common/EmpImage";
 import ImageUp from "../modal/ImageUp";
 import { Link } from "react-router-dom";
 
-const UserProfile = ({ user }) => {
+const UserProfile = ({ user, handleRemovePhoto }) => {
   const [showImageUp, setShowImageUp] = useState(false);
 
   return (
@@ -12,7 +12,7 @@ const UserProfile = ({ user }) => {
       <React.Fragment>
         <Row>
           <Col md={3} xs={6}>
-            <Card>
+            <Card className="text-center mb-3 shadow">
               <Card.Body>
                 <EmpImage empPhoto={user.photoBytes} />
               </Card.Body>
@@ -20,6 +20,16 @@ const UserProfile = ({ user }) => {
                 <p>
                   <Link to={"#"} onClick={() => setShowImageUp(true)}>
                     사진 변경
+                  </Link>
+                </p>
+                <p>
+                  <Link
+                    to={"#"}
+                    {...(user.photoId
+                      ? { onClick: handleRemovePhoto }
+                      : { style: { cursor: "default", color: "grey" }})}
+                  >
+                    사진 제거
                   </Link>
                 </p>
                 <ImageUp
