a2192d8799afaf0fdf4003de118baf3582f47898
osparking <jbpark03@gmail.com>
2025-03-09 11:35:05 +0900

    image upload modal show/close state & handler

--- src/component/user/UserProfile.jsx
+++ src/component/user/UserProfile.jsx
@@ -3,6 +3,11 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import ImageUp from "../modal/ImageUp";
 
 const UserProfile = ({ user }) => {
+  const [showImageUp, setShowImageUp] = useState(false);
+
+  const handleShowImageUp = () => setShowImageUp(true);
+  const handleCloseImageUp = () => setShowImageUp(false);
+
   return (
     <Container>
       <React.Fragment>
