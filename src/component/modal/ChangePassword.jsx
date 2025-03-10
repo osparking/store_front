56b6b03fa5cd744236bb5ea88014556a95895063
osparking <jbpark03@gmail.com>
2025-03-10 10:17:12 +0900

    password value state - 3 kinds of

--- src/component/modal/ChangePassword.jsx
+++ src/component/modal/ChangePassword.jsx
@@ -4,6 +4,11 @@ import { eyeOff } from "react-icons-kit/feather";
 const ChangePassword = () => {
   const [type, setType] = useState("password");
   const { icon, setIcon } = useState(eyeOff);
+  const [pwds, setPwds] = useState({
+    curPwd: "",
+    newPwd: "",
+    cnfPwd: "",
+  });
   return <div>ChangePassword</div>;
 };
 
