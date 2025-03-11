bf1134e6261c456ea7e578bdc38522e967df68fd
osparking <jbpark03@gmail.com>
2025-03-11 14:42:12 +0900

    login action function and more

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -1,29 +1,58 @@
 import React, { useState } from "react";
 import {
-    Button,
-    Card,
-    Col,
-    Container,
-    Form,
-    InputGroup,
-    Row,
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  InputGroup,
+  Row,
 } from "react-bootstrap";
 import { BsLockFill, BsPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import AlertMessage from "../common/AlertMessage";
+import BsAlertHook from "../hook/BsAlertHook";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const {
+    successMsg,
+    setSuccessMsg,
+    alertSuccess,
+    setAlertSuccess,
+    errorMsg,
+    setErrorMsg,
+    alertError,
+    setAlertError,
+  } = BsAlertHook();
+
   const handleChange = (e) => {
     setCreden({ ...credentials, [e.target.name]: e.target.value });
   };
+
+  const actLogin = async (e) => {
+    e.preventDefault();
+    if (!credentials.email || !credentials.password) {
+      setErrorMsg("바른 이메일과 비밀번호를 입력하세요.");
+      setAlertError(true);
+      return;
+    }
+    try {
+      const data = await loginUser(credentials.email, credentials.password);
+    } catch (error) {
+      setErrorMsg(error.response.data.message);
+      setAlertError(true);
+    }
+  };
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
         <Col sm={6}>
           <Card>
             <Card.Body>
-              <Card.Title className="text-center mb-4"></Card.Title>
-              <Form>
+              <Card.Title className="text-center mb-4">로그인</Card.Title>
+              <Form onSubmit={actLogin}>
                 <Form.Label>이메일</Form.Label>
                 <InputGroup>
                   <InputGroup.Text>
