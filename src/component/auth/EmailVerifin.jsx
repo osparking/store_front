1bfa1a4695297e7d2e10cdb7591a2873f44db6ae
박종범 <jbpark03@gmail.com>
2025-03-27 20:02:59 +0900

    email address verifin req' event handler

--- src/component/auth/EmailVerifin.jsx
+++ src/component/auth/EmailVerifin.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import { verifyEmail } from './AuthService';
 
 const EmailVerifin = () => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [verifyMsg, setVerifyMsg] = useState("이메일 검증 중입니다...");
+  const [alertType, setAlertType] = useState("alert-info");
+
+  const callVerifyEmail = async (token) => {
+    setIsProcessing(true);
+    try {
+      const response = await verifyEmail(token);
+
+      switch (response.message) {
+        case "계정 활성화":
+          setVerifyMsg("이메일 검증이 성공하여 로그인이 가능합니다.");
+          setAlertType("alert-success");
+          break;
+        case "이미 검증된 토큰":
+          setVerifyMsg("이메일 검증이 이미 완료된 바 있습니다.");
+          setAlertType("alert-info");
+          break;
+        default:
+          setVerifyMsg("이메일 검증 중 오류가 발생하였습니다.");
+          setAlertType("alert-danger");
+          break;
+      }
+    } catch (error) {
+      console.error("오류:", error);
+    }
+  };
   return (
     <div>EmailVerifin</div>
   )
