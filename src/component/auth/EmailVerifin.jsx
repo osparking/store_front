528e29d7af9db357f78007e695b97e611ae95847
박종범 <jbpark03@gmail.com>
2025-03-27 20:13:35 +0900

    verifin req' event handle error processing

--- src/component/auth/EmailVerifin.jsx
+++ src/component/auth/EmailVerifin.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { verifyEmail } from './AuthService';
 
 const EmailVerifin = () => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [verifyMsg, setVerifyMsg] = useState("이메일 검증 중입니다...");
   const [alertType, setAlertType] = useState("alert-info");
+  const [tokenExpired, setTokenExpired] = useState(false);
+  const [email, setEmail] = useState("");
 
   const callVerifyEmail = async (token) => {
     setIsProcessing(true);
@@ -25,9 +27,24 @@ const EmailVerifin = () => {
           setAlertType("alert-danger");
           break;
       }
-    } catch (error) {
-      console.error("오류:", error);
+  } catch (error) {
+    if (error.response) {
+      const message = error.response.data.message;
+      setEmail(error.response.data.data);
+      setAlertType("alert-danger");
+      if (message === "만료된 토큰") {
+        setVerifyMsg("계정 등록 때 발급된 토큰이 만료되었습니다.");
+        setAlertType("alert-warning");
+        setTokenExpired(true);
+      } else {
+        setVerifyMsg(message);
+      }
+    } else {
+      setVerifyMsg("서버 연결 오류가 발생하였습니다.");
     }
+  } finally {
+    setIsProcessing(false);
+  }
   };
   return (
     <div>EmailVerifin</div>
