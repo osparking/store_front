83419e59ddb1a290ff966c38e4af590cb2db73db
박종범 <jbpark03@gmail.com>
2025-03-30 19:53:15 +0900

    display count argu' value and more

--- src/component/card/CardCompo.jsx
+++ src/component/card/CardCompo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const CardCompo = ({ label }) => {
+const CardCompo = ({ label, count, IconCompo }) => {
     return (
         <div className="admin-card">
-            <div className="card-inner">{label}</div>
+            <div className="card-inner">{label}
+                <IconCompo className="card-icon" />
+            </div>
+            <h3 className='text-center'>{count}</h3>
         </div>
     )
 }
