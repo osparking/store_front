dfd402aeec31111a065d2d92ba9d8979e3601e35
박종범 <jbpark03@gmail.com>
2025-04-01 05:38:05 +0900

    handle admin overview tab click

--- src/component/admin/AdminSideBar.jsx
+++ src/component/admin/AdminSideBar.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { FaChartPie } from "react-icons/fa";
+import { GiOlive } from 'react-icons/gi';
 
-const AdminSideBar = () => {
+const AdminSideBar = ({ tabClicked  }) => {
     return (
         <aside id="sidebar" className="sidebar-responsive">
             <div className="sidebar-title">
-                범이비누 관리
+                <div className='sidebar-brand'>
+                    <GiOlive className="icon-header" />
+                    범이비누 관리
+                </div>
             </div>
             <ul className="sidebar-list">
-                <li className="sidebar-list-item">통계 및 차트</li>
+                <li className="sidebar-list-item"
+                    onClick={() => tabClicked("Overview")}
+                >
+                    <a href="#">
+                        <FaChartPie className="icon" />
+                        통계 및 차트
+                    </a>
+                </li>
                 <li className="sidebar-list-item">직원 관리</li>
                 <li className="sidebar-list-item">고객 관리</li>
+
             </ul>
         </aside>
     );
