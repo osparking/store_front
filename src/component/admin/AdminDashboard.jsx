f5add1f12814f3a2b5f4f1f6dcab182fbeb8ce7d
박종범 <jbpark03@gmail.com>
2025-04-01 05:30:51 +0900

    handles admin tab selection w/ sidebar

--- src/component/admin/AdminDashboard.jsx
+++ src/component/admin/AdminDashboard.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Overview from './Overview';
 import AdminSideBar from './AdminSideBar';
 
 const AdminDashboard = () => {
+  const [adminTab, setAdminTab] = useState("Overview");
+  const tabClicked = (tab) => {
+    setAdminTab(tab);
+  };
+
   return (
     <main className="admin-body">
       <div className="grid-container">
-        <AdminSideBar />
-        <div className="main-container">
-          <Overview />
+        <AdminSideBar
+          tabClicked={tabClicked} />
+
+        <div className="main-container" >
+          {adminTab === "Overview" && <Overview />}
         </div>
       </div>
     </main>
