f8dd5b4ef9e9a7743aadbe532dbcc693893ca412
박종범 <jbpark03@gmail.com>
2025-04-01 21:28:29 +0900

    axios api - read worker list from backend

--- src/component/worker/WorkerService.js
+++ src/component/worker/WorkerService.js
@@ -1,5 +1,29 @@
 import { api } from "../util/api";
 
+export async function getWorkerList() {
+  try {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const result = await axios({
+        method: "get",
+        url: `${prefix}/admin/worker/get_all`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return result.data;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    if (err.response.status === HttpStatusCode.Forbidden) {
+      return null;
+    } else {
+      throw err;
+    }
+  }
+}
+
 export async function getAllDept() {
   try {
     const result = await api.get("/worker/get_all_dept");
