6c995654150dfe7e47b58039c80e688308d36aee
박종범 <jbpark03@gmail.com>
2025-04-01 20:52:34 +0900

    make standard success/error const and setter

--- src/component/admin/WorkerTable.jsx
+++ src/component/admin/WorkerTable.jsx
@@ -4,6 +4,16 @@ const WorkerTable = () => {
   const [workerList, setWorkerList] = useState([]);
   const [showDelModal, setShowDelModal] = useState(false);
   const [workerToDel, setWorkerToDel] = useState(null);
+  const {
+    successMsg,
+    setSuccessMsg,
+    alertSuccess,
+    setAlertSuccess,
+    errorMsg,
+    setErrorMsg,
+    alertError,
+    setAlertError,
+  } = BsAlertHook();
 
   return (
     <div>일꾼 테이블</div>
