af6e7ff14b3f5bbe9b2bf7ba168aad4321fbe03e
박종범 <jbpark03@gmail.com>
2025-04-03 08:43:23 +0900

    deletes user account - API function

--- src/component/user/UserService.js
+++ src/component/user/UserService.js
@@ -3,6 +3,30 @@ import { api } from "../util/api";
 
 const prefix = "http://localhost:9193/api/s1";
 
+export async function deleteUserAccount(userId) {
+  try {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const result = await axios({
+        method: "delete",
+        url: `${prefix}/user/${userId}/delete`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return result.data;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    if (err.response.status === HttpStatusCode.Forbidden) {
+      return null;
+    } else {
+      throw err;
+    }
+  }  
+}
+
 export const toggleEnabledColumn = async (userId) => {
   try {
     const token = localStorage.getItem("token");
