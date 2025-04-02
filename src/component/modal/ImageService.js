5867816b822f42cef072201148d4841c07d4edc0
박종범 <jbpark03@gmail.com>
2025-04-02 09:37:40 +0900

    sends JWT token with photo upload request

--- src/component/modal/ImageService.js
+++ src/component/modal/ImageService.js
@@ -1,16 +1,32 @@
 import React from "react";
 import { api } from "../util/api";
+import axios, { HttpStatusCode } from "axios";
+
+const prefix = "http://localhost:9193/api/s1";
 
 export async function uploadEmpPhoto(userId, file) {
   try {
     const formData = new FormData();
     formData.append("empId", userId);
     formData.append("file", file);
-
-    const response = await api.post("/photo/upload", formData);
+  const token = localStorage.getItem("token");
+  if (token) {
+    const response = await axios({
+      method: "post",
+      url: `${prefix}/photo/upload`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: formData
+    });
     return response.data;
-  } catch (error) {
-    throw error;
+  }
+} catch (err) {
+  if (err.response.status === HttpStatusCode.Forbidden) {
+    return null;
+  } else {
+    throw err;
+  }
   }
 }
 
