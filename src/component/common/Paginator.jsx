43a5e1f3f0ca0b7c0bbe43129e2904a980f2e344
박종범 <jbpark03@gmail.com>
2025-04-04 07:57:42 +0900

    pagination component w/ 4 formal params

---
+++ src/component/common/Paginator.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Pagination } from "react-bootstrap";
+
+const Paginator = ({ pageSize, totalItems, currPage, setCurrPage }) => {
+  let active = currPage;
+  let items = [];
+  for (let i = 1; i <= Math.ceil(totalItems / pageSize); i++) {
+    items.push(
+      <Pagination.Item
+        key={i}
+        active={i === active}
+        onClick={() => setCurrPage(i)}
+      >
+        {i}
+      </Pagination.Item>
+    );
+  }
+  return (
+    <div className="d-flex justify-content-end">
+      <Pagination>{items}</Pagination>
+    </div>
+  );
+};
+
+export default Paginator;
