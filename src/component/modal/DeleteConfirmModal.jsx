808e298cff791b6a56e0cc5c60da115ca6809fef
박종범 <jbpark03@gmail.com>
2025-04-11 07:31:58 +0900

    while being deleted, disables delete button

--- src/component/modal/DeleteConfirmModal.jsx
+++ src/component/modal/DeleteConfirmModal.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 import { Button, Modal } from 'react-bootstrap';
 
 const DeleteConfirmModal = ({
-    show,
-    onHide,
-    handleDeletion,
-    target
+  show,
+  onHide,
+  handleDeletion,
+  target,
+  disabled,
 }) => {
-    return (
-        <Modal show={show} onHide={onHide} >
-            <Modal.Header closeButton>
-                <Modal.Title>삭제 후 복구 불가</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                '{target}' 계정 삭제는 복구 불가합니다!
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={onHide}>
-                    취소
-                </Button>
-                <Button variant="danger" onClick={handleDeletion}>
-                    삭제
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={show} onHide={onHide} >
+      <Modal.Header closeButton>
+        <Modal.Title>삭제 후 복구 불가</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        '{target}' 계정 삭제는 복구 불가합니다!
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          삭제 취소
+        </Button>
+        <Button variant="danger" onClick={handleDeletion} disabled={disabled}>
+          {disabled ? "진행 중~" : "삭제 진행"}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default DeleteConfirmModal
