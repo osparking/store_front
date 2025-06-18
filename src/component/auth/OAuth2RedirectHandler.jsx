15bed14c6baa3528f4821f305c0a22016506d9ef
박종범 <jbpark03@gmail.com>
2025-06-18 11:44:15 +0900

    handle JWT missing and decode errors

--- src/component/auth/OAuth2RedirectHandler.jsx
+++ src/component/auth/OAuth2RedirectHandler.jsx
@@ -10,8 +10,16 @@ const OAuth2RedirectHandler = () => {
     const token = params.get("token");
 
     if (token) {
-      const decodedToken = jwtDecode(token);
-      console.log("JWT:", decodedToken);
+      try {
+        const decodedToken = jwtDecode(token);
+        console.log("Decoded Token:", decodedToken);
+      } catch (error) {
+        console.error('토큰 해독 오류:', error);
+        navigate('/login');
+      }
+    } else {
+      console.log("오류: URL 중 토큰 부재");
+      navigate('/login');
     }
   }, []);
   return <div>소셜 로그인 재방향...</div>;
