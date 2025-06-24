edc4c651e6b9324a23fa8e977b6d32a8f104ee51
박종범 <jbpark03@gmail.com>
2025-06-24 16:15:13 +0900

    on button click, reveal QR code

--- src/component/auth/QRcodeBox.jsx
+++ src/component/auth/QRcodeBox.jsx
@@ -3,15 +3,19 @@ import { useState } from "react";
 
 const QRcodeBox = ({ qrCodeUrl, setTwoFaEnabled, setShowQrCode }) => {
   const [qrRevealed, setQrRevealed] = useState(false);
+  const [beingVerified, setBeingVerified] = useState(false);
+  const [code, setCode] = useState("");
 
-  const showDetails = () => {
-    setRevealQR(true);
+  const revealQRcode = () => {
+    setQrRevealed(true);
   };
+
+  const verify2FA = async () => {};
 
   return (
     <div className="py-3">
       <h5 className="font-bold text-slate-700">
-        <button onClick={showDetails}>
+        <button onClick={revealQRcode}>
           QR 코드 -{" "}
           {qrRevealed ? (
             "스캔할 것"
@@ -22,7 +26,30 @@ const QRcodeBox = ({ qrCodeUrl, setTwoFaEnabled, setShowQrCode }) => {
           )}
         </button>
       </h5>
-      <div>{qrRevealed && <div></div>}</div>
+      <div>
+        {qrRevealed && (
+          <div>
+            <img src={qrCodeUrl} alt="QR 코드" />
+            <div className="flex items-center  gap-1  mt-4">
+              <input
+                type="text"
+                placeholder="(구글 인증자 코드 입력)"
+                value={code}
+                required
+                className="mt-4 border px-2 py-1 border-slate-800 rounded-md"
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <button
+                disabled={beingVerified}
+                className="bg-btnColor text-red  px-3 h-10 rounded-md mt-4"
+                onClick={verify2FA}
+              >
+                인증 코드 검증
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
