8c4130007cf77769d91e346f8ff5bd886a0c08db
박종범 <jbpark03@gmail.com>
2025-07-03 20:51:29 +0900

    soap ingredients info data file

---
+++ src/component/soaps/data.js
@@ -0,0 +1,41 @@
+export const ingredients = [
+  {
+    id: 0,
+    imageFile: "olive-oil.jpg",
+    name: "올리브 오일 엑스트라 버진",
+    effect:
+      "비타민 E와 항산화 성분은 피부 노화를 방지하고, " +
+      "피부 탄력을 유지하는 데 도움을 줍니다. 또한, 보습 " +
+      "효과가 뛰어나 건조한 피부를 촉촉하게 가꾸어 줍니다. ",
+    source: "구글 검색 결과",
+    searchDate: "2025-07-03",
+    searchKey: "올리브오일 엑스트라버진 효능",
+    imageId: "MK3eW3A",
+  },
+  {
+    id: 1,
+    imageFile: "eosung-plant-root-powder.jpg",
+    name: "어성초",
+    effect:
+      "어성초는 수분을 유지하는 기능과 피부의 보습에 도움을 크게 주어..." +
+      "...피부의 순환을 촉진시켜 피부를 맑게 해주는데 도움을..." +
+      "피부의 진정, 깨끗한 피부에 도움을 줍니다...원래는 약모밀이라는 약초...",
+    source: "수제나라",
+    searchDate: "2025-07-03",
+    searchKey: "어성초분말",
+    imageId: "mynHUSa",
+  },
+  {
+    id: 2,
+    imageFile: "yulmoo.jpg",
+    name: "율무분말",
+    effect:
+      "율무는 항산화 효과와 더불어 색소침착 억제에 도움을 주어 피부톤 개선에" + 
+      " 좋습니다. 또한 트러블을 진정 시키는데 사용했던 곡물로 알려져 있어 " + 
+      "피부 진정, 트러블 감소, 모공 관리에 도움을 줍니다." ,
+    source: "수제나라",
+    searchDate: "2025-07-03",
+    searchKey: "율무",
+    imageId: "bE7W1ji",
+  },
+];
