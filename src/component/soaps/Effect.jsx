091cb9683c0b682a0f8c291b3c8d8877a87a39ec
박종범 <jbpark03@gmail.com>
2025-07-06 10:22:03 +0900

    Container - Row - Col structure used

--- src/component/soaps/Effect.jsx
+++ src/component/soaps/Effect.jsx
@@ -1,8 +1,9 @@
 import { Col, Container, Row } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
 import "../../index.css";
 import { ingredients } from "./data.js";
 import "./effect.css";
-import { Link } from "react-router-dom";
 
 export default function Effect() {
   const imageRoot = "/src/assets/images/ingred";
@@ -15,46 +16,68 @@ export default function Effect() {
     } else if (ingredName.indexOf("율무") !== -1) {
       color = "#2c1e13";
     }
-    return {"color" : color};
+    return { color: color };
   }
 
   function Sujenara() {
     return (
-      <Link to="https://sujaenara.com/" target="_blank">수제나라</Link>      
-    )
+      <Link to="https://sujaenara.com/" target="_blank">
+        수제나라
+      </Link>
+    );
   }
 
-  const listItems = ingredients.map((ingred) => (
-    <li key={ingred.id}>
-      <img src={`${imageRoot}/${ingred.imageFile}`} alt={ingred.name} />
-      <p>
-        <b>{ingred.name}:</b>
-        <span style={ingredColor(ingred.name)}>{" " + ingred.effect}</span>
-        <hr />
-        <small>
-          출처 - {ingred.source === "수제나라" ? Sujenara(): ingred.source}, 
-          검색키 -{" "}
-          {ingred.searchKey}, 검색일 - {ingred.searchDate}
-        </small>
-      </p>
+  const listItems = ingredients.map((ingred, idx) => (
+    <li key={idx}>
+      <Container>
+        <Row className="align-items-center">
+          {" "}
+          {/* Use align-items-center for vertical alignment */}
+          <Col xs={12} md={6}>
+            {" "}
+            {/* Column for the image */}
+            <Image
+              src={`${imageRoot}/${ingred.imageFile}`}
+              fluid
+              alt={ingred.name}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            {" "}
+            {/* Column for the paragraph */}
+            <p className="inline">
+              <b>{ingred.name}:</b>
+              <span style={ingredColor(ingred.name)}>
+                {" " + ingred.effect}
+              </span>
+              <br />
+              <small>
+                출처 -{" "}
+                {ingred.source === "수제나라" ? Sujenara() : ingred.source},
+                검색키 - {ingred.searchKey}, 검색일 - {ingred.searchDate}
+              </small>
+            </p>
+          </Col>
+        </Row>
+      </Container>
       <hr />
     </li>
   ));
 
-  return (
-    <Container fluid className="home-container mt-3">
-      <Row className="justify-content-center allIngred">
-        <Col md={8}>
-          <div>
-            <h2>
-              <small>재료에서 유래하는... </small>
-              <br />
-              <strong>비누 효능</strong>
-            </h2>
-            <ul className="noBullet">{listItems}</ul>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+return (
+  <Container fluid className="home-container ">
+    <Row className="justify-content-center allIngred">
+      <Col md={8}>
+        <div>
+          <h2 className="mt-3 mb-1">
+            <small>재료에서 유래하는...</small>
+            <strong> 비누 효능</strong>
+          </h2>
+          <hr />
+          <ul className="noBullet">{listItems}</ul>
+        </div>
+      </Col>
+    </Row>
+  </Container>
+);
 }
