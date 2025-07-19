df3f2d4b0afe2e102d3a3dc98c2212b2990fb723
박종범 <jbpark03@gmail.com>
2025-07-19 09:34:26 +0900

    generic soap shape carousel jsx completed

--- src/component/soaps/SoapImages.jsx
+++ src/component/soaps/SoapImages.jsx
@@ -26,6 +26,77 @@ const SoapImages = ({soapImages, bgColor, indColor}) => {
   const [slide, setSlide] = useState(0);
 
   return (
+    <Row className="justify-content-center allIngred pt-3 mb-5">
+      <Col md={8}>
+        <h2 className="ps-0 mb-4" id="normal-soap">
+          <strong>보통비누</strong>
+        </h2>
+        <div className="carousel-container">
+          <div className="carousel">
+            <button
+              className="button button-left"
+              style={buttonStyle}
+              onClick={() => nextSlide("L")}
+              disabled={arrowDisabled}
+            >
+              <BsArrowLeftCircleFill style={arrowStyle} />
+            </button>
+            {soapImages.map((soap, idx) => {
+              return (
+                <img
+                  key={idx}
+                  style={{
+                    backgroundColor: "#263e59",
+                    width: "100%",
+                    height: "auto",
+                  }}
+                  src={`${imageRoot}/${soap.image}`}
+                  alt={soap.name}
+                  className={
+                    slide === idx
+                      ? "slide carousel"
+                      : "slide carousel slide-hidden"
+                  }
+                />
+              );
+            })}
+            <button
+              className="button button-right"
+              style={buttonStyle}
+              onClick={() => nextSlide("R")}
+              disabled={arrowDisabled}
+            >
+              <BsArrowRightCircleFill style={arrowStyle} />
+            </button>
+            <span className="indicators">
+              {soapImages.map((_, idx) => {
+                return (
+                  <button
+                    key={idx}
+                    onClick={() => setSlide(idx)}
+                    className={
+                      slide === idx
+                        ? "indicator"
+                        : "indicator indicator-inactive"
+                    }
+                    style={{
+                      backgroundColor: slide === idx ? selColor : "#6199daff",
+                    }}
+                  />
+                );
+              })}
+            </span>
+          </div>
+        </div>
+        <div className="imgCapDiv">
+          <Figure className="mt-3">
+            <Figure.Caption className="soapCap">
+              <strong>{soapImages[slide].desc}</strong>
+            </Figure.Caption>
+          </Figure>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
