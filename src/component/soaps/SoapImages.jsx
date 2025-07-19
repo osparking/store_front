d29480f929310e00256d4104daa97d3abddfdbb9
박종범 <jbpark03@gmail.com>
2025-07-19 09:27:45 +0900

    put props and a few names for generic soap carousel

--- src/component/soaps/SoapImages.jsx
+++ src/component/soaps/SoapImages.jsx
@@ -1,6 +1,31 @@
-const SoapImages = () => {
-  return  (
-    <div></div>
+import React, { useState } from "react";
+import { Col, Figure, Row } from "react-bootstrap";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+const SoapImages = ({soapImages, bgColor, indColor}) => {
+  const arrowSz = 1.8; // in rem
+
+  const buttonStyle = {
+    position: "absolute",
+    marginTop: `${-(arrowSz)}rem`,
+    zIndex: "1",
+    top: "50%",
+    background: "none",
+    border: "none",
+    cursor: "pointer",
+    fontSize: `${arrowSz}rem`,
+  };
+
+  const [arrowDisabled, setArrowDisabled] = useState(false);
+  const selColor = "#d9c1a6";
+  const arrowStyle = {
+    color: selColor,
+  };
+
+  const imageRoot = "/src/assets/images/soap";
+  const [slide, setSlide] = useState(0);
+
+  return (
   );
 };
 
