6d9261fd64e6949c71c9c15535860b0341237c40
박종범 <jbpark03@gmail.com>
2025-07-26 08:30:58 +0900

    error fix - isWorker is not defined

--- src/component/common/JwtUtils.js
+++ src/component/common/JwtUtils.js
@@ -5,6 +5,7 @@ export const jwtToUser = (jwt) => {
   console.log("Decoded Token:", decodedToken);
 
   let isAdmin = decodedToken.roles.includes("ROLE_ADMIN");
+  let isWorker = decodedToken.roles.includes("ROLE_WORKER");  
 
   const user = {
     id: decodedToken.id,
