8637c616d4748ba42722739e08f90af7e53c0afe
박종범 <jbpark03@gmail.com>
2025-07-25 20:24:21 +0900

    adds route on Worker dashboard : /work_item

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,11 @@ import Login from "./component/auth/Login";
 import OAuth2RedirectHandler from "./component/auth/OAuth2RedirectHandler";
 import Home from "./component/home/Home";
 import RootLayout from "./component/layout/RootLayout";
+import SoapIntro from "./component/soaps/SoapIntro";
 import RegisterUser from "./component/user/RegisterUser";
 import UserUpdate from "./component/user/UpdateUser";
 import UserDashboard from "./component/user/UserDashboard";
-import SoapIntro from "./component/soaps/SoapIntro";
+import WorkerDashboard from "./component/worker/WorkerDashboard";
 
 function App() {
   const router = createBrowserRouter(
@@ -26,10 +27,11 @@ function App() {
         <Route path="/email_verifin" element={<EmailVerifin />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard/:id/user" element={<UserDashboard />} />
-        <Route path="/dashboard/admin" element={<AdminDashboard/>} />
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
         <Route path="/user/:id/update" element={<UserUpdate />} />
         <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
         <Route path="/soap_intro" element={<SoapIntro />} />
+        <Route path="/work_item" element={<WorkerDashboard />} />
       </Route>
     )
   );
