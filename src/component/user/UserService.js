46b693eb82a650d832dace7904b9ba1903e552ad
박종범 <jbpark03@gmail.com>
2025-07-26 10:49:48 +0900

    move callWithToken to a more general place

--- src/component/user/UserService.js
+++ src/component/user/UserService.js
@@ -1,45 +1,7 @@
 import axios, { HttpStatusCode } from "axios";
-import { api } from "../util/api";
+import { api, callWithToken } from "../util/api";
 
 const prefix = "http://localhost:9193/api/s1";
-
-export async function callWithToken(method, urlSuffix, data = null) {
-  try {
-    const token = localStorage.getItem("TOKEN");
-    if (token) {
-      console.log("url: ", `${prefix}${urlSuffix}`);
-      let config = {
-        method: method,
-        url: `${prefix}${urlSuffix}`,
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-      if (data) {
-        config = {
-          method: method,
-          url: `${prefix}${urlSuffix}`,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          data: data,
-        }
-      } 
-      const result = await axios(config);
-      return result;
-    } else {
-      return null;
-    }
-  } catch (err) {
-    if (err.response.status === HttpStatusCode.Forbidden ||
-      err.response.status === HttpStatusCode.Unauthorized
-    ) {
-      return null;
-    } else {
-      throw err;
-    }
-  }
-}
 
 export async function updateUser(userId, user) {
   try {
--- src/component/util/api.js
+++ src/component/util/api.js
@@ -3,3 +3,41 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:9193/api/s1",
 });
+
+export async function callWithToken(method, urlSuffix, data = null) {
+  try {
+    const token = localStorage.getItem("TOKEN");
+    if (token) {
+      console.log("url: ", `${prefix}${urlSuffix}`);
+      let config = {
+        method: method,
+        url: `${prefix}${urlSuffix}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+      if (data) {
+        config = {
+          method: method,
+          url: `${prefix}${urlSuffix}`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: data,
+        }
+      } 
+      const result = await axios(config);
+      return result;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    if (err.response.status === HttpStatusCode.Forbidden ||
+      err.response.status === HttpStatusCode.Unauthorized
+    ) {
+      return null;
+    } else {
+      throw err;
+    }
+  }
+}
