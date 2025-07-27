34b6ceead9f350a660470ffef571a25d0f638d25
박종범 <jbpark03@gmail.com>
2025-07-27 16:08:52 +0900

    default stored ingre info submit button

--- src/component/worker/StoIngEntry.jsx
+++ src/component/worker/StoIngEntry.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import BsAlertHook from "../hook/BsAlertHook";
 import { sendStoIngInfo } from "./WorkerService";
-import { Card, Col, Container, Form, Row } from "react-bootstrap";
 
 const StoIngEntry = () => {
   const [ingredient, setIngredient] = useState({
@@ -47,16 +47,27 @@ const StoIngEntry = () => {
     <Container className="mt-5">
       <Row className="justify-content-center">
         <Col xs={12} md={8} lg={6}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Card>
               <Card.Header></Card.Header>
-              <Card.Body></Card.Body>
+              <Card.Body>
+                <div className="d-flex justify-content-center mb-3 mt-3">
+                  <Button
+                    type="submit"
+                    variant="outline-primary"
+                    size="sm"
+                    className="me-2"
+                  >
+                    등록
+                  </Button>
+                </div>
+              </Card.Body>
             </Card>
           </Form>
         </Col>
       </Row>
-    </Container>  
-  )
+    </Container>
+  );
 };
 
 export default StoIngEntry;
