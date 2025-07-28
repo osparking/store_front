72ddbcfc0f05a215332433aa981ebdd91c4580e5
박종범 <jbpark03@gmail.com>
2025-07-29 07:40:27 +0900

    stored and expire date entry section

--- src/component/worker/StoIngEntry.jsx
+++ src/component/worker/StoIngEntry.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import DatePicker from "react-datepicker";
 import BsAlertHook from "../hook/BsAlertHook";
 import BuyPlaceSelector from "./BuyPlaceSelector";
 import IngreNameSelector from "./IngreNameSelector";
@@ -69,14 +70,45 @@ const StoIngEntry = () => {
                     </Col>
                   </Row>
                 </Form.Group>
-                <Form.Group as={Row} controlId="password" className="mb-4">
-                  <Col className="mb-2 mb-sm-0">
+                <Form.Group as={Row} controlId="buyPlace" className="mb-1">
+                  <Col className="mb-1 mb-sm-0">
                     <BuyPlaceSelector
                       buyPlace={ingredient.buyPlace}
                       onChange={handleChange}
                     />
                   </Col>
                 </Form.Group>
+                <Form.Group as={Row} controlId="twoDates" className="mb-1">
+                  <Row>
+                    <Col xs={6} className="mb-3 mb-sm-0">
+                      <Form.Label>입고 일자</Form.Label>
+                      <DatePicker
+                        selected={storeDate}
+                        onChange={(date) => setStoreDate(date)}
+                        dateFormat="yyyy-MM-dd"
+                        className="form-control"
+                        maxDate={new Date()}
+                        placeholderText="(입고 날짜)"
+                        defaultShow={true}
+                        required
+                      />
+                    </Col>
+                    <Col xs={6} className="mb-3 mb-sm-0">
+                      <Form.Label>사용 기한</Form.Label>
+                      <DatePicker
+                        selected={ingredient.expireDate}
+                        onChange={(date) => setStoreDate(date)}
+                        dateFormat="yyyy-MM-dd"
+                        className="form-control"
+                        maxDate={new Date()}
+                        placeholderText="(사용기한)"
+                        defaultShow={true}
+                        required
+                      />
+                    </Col>                    
+                  </Row>
+                </Form.Group>
+
                 <div className="d-flex justify-content-center mb-3 mt-3">
                   <Button
                     type="submit"
