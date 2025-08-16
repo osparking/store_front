81900529a6253e01b3223e6166ab3d9784c15c19
BumTiger <jbpark03@gmail.com>
2025-08-16 18:09:59 +0900

    use image only soap carousel in buy page

--- src/component/buy/BuySoap.jsx
+++ src/component/buy/BuySoap.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Col, Row } from "react-bootstrap";
+import { soapImages } from "../soaps/SoapImages.js";
+import SoapCarousel from "../soaps/SoapCarousel.jsx";
 
 const BuySoap = () => {
-  return (
-    <div>BuySoap</div>
-  )
-}
+  const normalSoaps = soapImages.filter((soap) => soap.shape === "normal");
+  const sWhiteSoaps = soapImages.filter((soap) => soap.shape === "s-white");
+  const maejooSoaps = soapImages.filter((soap) => soap.shape === "maejoo");
 
-export default BuySoap
+  return (
+    <div style={{ width: "100%" }}>
+      <Row className="align-items-center">
+        {" "}
+        {/* Use align-items-center for vertical alignment */}
+        <Col xs={12} md={6}>
+          <SoapCarousel
+            soapImages={normalSoaps}
+            bgColor="#263e59"
+            indColor="#6199daff"
+          />
+        </Col>
+        <Col xs={12} md={6}>
+          {" "}
+          {/* Column for the paragraph */}
+          <p className="inline">외형</p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default BuySoap;
