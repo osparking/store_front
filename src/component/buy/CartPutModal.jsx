c05b17340995ee61912f1452989a1373758e65c9
BumTiger <jbpark03@gmail.com>
2025-08-25 16:44:15 +0900

    cart save result display component, etc

---
+++ src/component/buy/CartPutModal.jsx
@@ -0,0 +1,54 @@
+import { Button, Modal } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const CartPutModal = ({ show, closer, getResultString = [] }) => {
+  console.log("type: ", typeof getResultString);
+  console.log("getResultString: ", getResultString);
+  const navigate = useNavigate();
+
+  const showShoppingCart = () => {
+    navigate("/shopping_cart", { state: { showCart: true } });
+  };
+
+  return (
+    <Modal show={show} onHide={closer}>
+      <Modal.Header closeButton>
+        <Modal.Title>장바구니 담은 결과</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h5>변경 내용 요약</h5>
+        <ul>
+          {getResultString.length > 0 ? (
+            getResultString.map((item, index) => <li key={index}>{item}</li>)
+          ) : (
+            <li>장바구니에 담긴 상품이 없습니다</li>
+          )}
+        </ul>
+        <div
+          style={{ display: "flex", gap: "20px" }}
+          className="justify-content-center"
+        >
+          <Button
+            variant="info"
+            size="sm"
+            className="pt-2 pb-2 order-button-width"
+            onClick={showShoppingCart}
+          >
+            장바구니 보기
+          </Button>
+          <Button
+            variant="success"
+            size="sm"
+            className="pt-2 pb-2 order-button-width"
+            onClick={closer}
+          >
+            뒤로 돌아가기
+          </Button>
+        </div>
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CartPutModal;
---
+++ src/component/buy/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShoppingCart = () => {
+  return (
+    <div className="d-flex justify-content-center" style={{ marginTop: "40%" }}>
+      <h1 style={{ color: "white" }}>ShoppingCart</h1>
+    </div>
+  );
+};
+
+export default ShoppingCart;
