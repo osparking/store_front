551062ef68742439241807986a61481c6d41bf58
BumTiger <jbpark03@gmail.com>
2025-08-25 19:40:53 +0900

    shopping cart content page - skeleton

---
+++ src/component/buy/ShoppingCart.jsx
@@ -0,0 +1,9 @@
+const ShoppingCart = () => {
+  return (
+    <div className="d-flex justify-content-center" style={{ marginTop: "40%" }}>
+      <h1 style={{ color: "white" }}>ShoppingCart</h1>
+    </div>
+  );
+};
+
+export default ShoppingCart;
