2bd1131b5b2c98831b702a6df28a7f204e96d953
BumTiger <jbpark03@gmail.com>
2025-08-27 18:41:50 +0900

    serve the cart of a user

--- src/component/buy/orderService.js
+++ src/component/buy/orderService.js
@@ -1,9 +1,19 @@
-import { api } from "../util/api";
+import { api, callWithToken } from "../util/api";
 
 export async function getSoapShapes() {
   try {
     const result = await api.get('/soap/shapes');
     return result.data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function readUserCart(userId) {
+  try {
+    const result = await callWithToken("get", `/cart/${userId}/get`);
+    console.log("data: ", result.data.data);
+    return result.data.data;
   } catch (err) {
     throw err;
   }
