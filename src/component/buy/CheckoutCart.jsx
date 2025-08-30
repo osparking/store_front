90a1ae1aeef3144b2c2210208b7851a4c8ab3e6e
BumTiger <jbpark03@gmail.com>
2025-08-30 11:02:33 +0900

    checkout items and grand total

---
+++ src/component/buy/CheckoutCart.jsx
@@ -0,0 +1,43 @@
+import { Col, Row, Table } from "react-bootstrap";
+import "./checkoutCart.css";
+
+const CheckoutCart = ({ productList, grandTotal }) => {
+
+  return (
+    <Row className="justify-content-center mt-3 pt-4 rowStyle">
+      <Col md={3} className="d-flex align-items-center listLeftLabel">
+        <div className="container">
+          <p className="rightColumnParagraph">주문 상품 요약 ➡</p>
+        </div>
+      </Col>
+      <Col md={5}>
+        <Table bordered hover striped>
+          <thead>
+            <tr>
+              <th>외형명</th>
+              <th>수량</th>
+              <th>소계</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productList &&
+              productList.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.shapeLabel}</td>
+                  <td>{item.count}</td>
+                  <td className="cartItem">
+                    {item.subTotal.toLocaleString()} 원
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan={3} className="grandTotal">합계 : {grandTotal} 원</td>
+              </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
+};
+
+export default CheckoutCart;
