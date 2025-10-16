4e934eb198b2464dc1ed21627353312627b5f9fe
BumTiger <jbpark03@gmail.com>
2025-10-17 06:17:46 +0900

    full delivery company name specified

--- src/component/buy/Payment.jsx
+++ src/component/buy/Payment.jsx
@@ -33,7 +33,7 @@ const Payment = () => {
               <li>배송비: {paymentData.deliveryFee.toLocaleString()} 원</li>
               <li>결제 금액: {paymentData.paymentFee.toLocaleString()} 원</li>
             </ul>
-            <li>배송사: GS 포스트박스 편의점택배</li>
+            <li>지정 배송사: GS 포스트박스 편의점택배 협력사 CJ대한통운(오네)</li>
             <li>결제 수단</li>
           </ul>
         </div>
