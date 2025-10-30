baa5038fe697447c12a60a34ae9a16851d5e93f5
BumTiger <jbpark03@gmail.com>
2025-10-30 09:35:34 +0900

    uses 3 properties received from Widget

--- src/component/buy/OrderDigest.jsx
+++ src/component/buy/OrderDigest.jsx
@@ -1,15 +1,11 @@
 import { Col, Row } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 
-const OrderDigest = () => {
-  const location = useLocation();
-  const { paymentData } = location.state || false;
-  const recepient = paymentData.recipRegiReq;
-
+const OrderDigest = ({ name, amount, address }) => {
   return (
     <Row className="d-flex justify-content-center">
       <Col md={8}>
-        {paymentData && (
+        {name && (
           <div
             md={8}
             className=" ms-5 me-5"
@@ -17,9 +13,9 @@ const OrderDigest = () => {
           >
             <h5>주문 요약</h5>
             <ul>
-              <li>주문 이름 : {paymentData.orderName}</li>
-              <li>결제 금액 : {paymentData.paymentFee.toLocaleString()} 원</li>
-              <li>상세 주소 : {recepient.addressDetail}</li>
+              <li>주문 이름 : {name}</li>
+              <li>결제 금액 : {amount.toLocaleString()} 원</li>
+              <li>상세 주소 : {address}</li>
             </ul>
           </div>
         )}
