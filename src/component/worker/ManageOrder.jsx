35f63a52b77bc219a337b2fb34d9492e0db8da7e
BumTiger <jbpark03@gmail.com>
2025-11-23 13:33:50 +0900

    pass props to 2 sub components

--- src/component/worker/ManageOrder.jsx
+++ src/component/worker/ManageOrder.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "../../App.css";
 import OrderTable from "./order/OrderTable";
+import OrderDetail from "./order/OrderDetail";
 
 const ManageOrder = () => {
   const [showDetail, setShowDetail] = useState(false);
+  const [detailId, setDetailId] = useState(0);
 
   return (
     <main>
       <h5 className="chart-title">주문 관리</h5>
-      {!showDetail && <OrderTable />}
+      {showDetail ? (
+        <OrderDetail detailId = {detailId} setShowDetail={setShowDetail} />
+      ) : (
+        <OrderTable setShowDetail={setShowDetail} setDetailId={setDetailId} />
+      )}
     </main>
   );
 };
