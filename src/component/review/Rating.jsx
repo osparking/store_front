1ec2a8a1b9884bfd54613adc47061635154bff8d
BumTiger <jbpark03@gmail.com>
2025-12-30 21:02:40 +0900

    리뷰 일시 및 작성자를 제2, 제3 Col로 추가

--- src/component/review/Rating.jsx
+++ src/component/review/Rating.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Container, Form } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import { FaStar } from "react-icons/fa";
+import { formatDate } from "../util/utilities";
 import "./Rating.css";
 
-const Rating = ({ stars, setStars, editable }) => {
+const Rating = ({ stars, setStars, editable, review }) => {
   const [hover, setHover] = useState(null);
   const handleRatingChange = (value) => {
     if (editable) setStars(value);
@@ -11,33 +12,46 @@ const Rating = ({ stars, setStars, editable }) => {
 
   return (
     <Container>
-      <h5 style={{ textAlign: "left" }}>별점 평가</h5>
-      <div className="mb-3">
-        {[...Array(5)].map((_, index) => {
-          const starCount = index + 1;
-          return (
-            <Form.Label key={index} className="me-2">
-              <Form.Check
-                type="radio"
-                name="rating"
-                value={starCount}
-                onChange={() => handleRatingChange(starCount)}
-                checked={stars === starCount}
-                inline
-              />
-              <FaStar
-                size={20}
-                className={editable ? "star" : "star no-cursor"}
-                color={starCount <= (hover || stars) ? "#ffc107" : "#e4e5e9"}
-                onMouseEnter={() => setHover(editable && starCount)}
-                onMouseLeave={() => setHover(null)}
-              />
-            </Form.Label>
-          );
-        })}
-      </div>
+      <Row className="align-items-center">
+        <Col xs={4} md={4}>
+          <h5 style={{ textAlign: "left" }}>별점 평가</h5>
+          <div className="mb-3">
+            {[...Array(5)].map((_, index) => {
+              const starCount = index + 1;
+              return (
+                <Form.Label key={index} className="me-2">
+                  <Form.Check
+                    type="radio"
+                    name="rating"
+                    value={starCount}
+                    onChange={() => handleRatingChange(starCount)}
+                    checked={stars === starCount}
+                    inline
+                  />
+                  <FaStar
+                    size={20}
+                    className={editable ? "star" : "star no-cursor"}
+                    color={
+                      starCount <= (hover || stars) ? "#ffc107" : "#e4e5e9"
+                    }
+                    onMouseEnter={() => setHover(editable && starCount)}
+                    onMouseLeave={() => setHover(null)}
+                  />
+                </Form.Label>
+              );
+            })}
+          </div>
+        </Col>
+        <Col className="text-center" xs={4} md={4}>
+          <span>리뷰 일시: {formatDate(review.reviewTime)}</span>
+        </Col>
+        <Col className="text-end" xs={4} md={4}>
+          <span className="me-2">작성자: {review.customerName}</span>
+        </Col>
+      </Row>
     </Container>
   );
 };
 
 export default Rating;
+("");
