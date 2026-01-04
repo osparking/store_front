b56958f9c3d7309eb1d82e448bb5e574497c573f
BumTiger <jbpark03@gmail.com>
2026-01-05 07:44:06 +0900

    자료 부재인 경우 표시되는 성분 간소화

--- src/component/common/NoDataExists.jsx
+++ src/component/common/NoDataExists.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import "./NoDataExists.css";
 
-const NoDataExists = ({ dataType, errorMessage }) => {
-    return (
-        <div className="text-center mt-5">
-            <h4>현재 {dataType}가/이 존재하지 않습니다.</h4>
-            {errorMessage && <p className="text-danger">{errorMessage}</p>}
-        </div>
-    );
-}
+const NoDataExists = ({ dataType }) => {
+  return (
+    <div className="center-container">
+      <h6>{dataType}가 없습니다.</h6>
+    </div>
+  );
+};
 
-export default NoDataExists
+export default NoDataExists;
