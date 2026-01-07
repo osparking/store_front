9c77feaf93eb59192b222515c5aac2fd8d31de13
BumTiger <jbpark03@gmail.com>
2026-01-07 15:52:15 +0900

    고의로 1초 지연하는 부분 주석화

--- src/component/user/question/QuestionService.js
+++ src/component/user/question/QuestionService.js
@@ -2,7 +2,7 @@ import { callWithToken } from "../../util/api";
 
 export async function saveQuestion(data) {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
     const result = await callWithToken("post", "/question/add", data);
     console.log("QuestionService: ", result.data);
     return result.data;
