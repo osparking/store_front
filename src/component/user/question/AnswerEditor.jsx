a9075a20440513d3a97ded2ea0b33499302b87e2
BumTiger <jbpark03@gmail.com>
2026-01-11 21:15:20 +0900

    범이 답변 편집기 성분 - 저장 함수 구현 전

---
+++ src/component/user/question/AnswerEditor.jsx
@@ -0,0 +1,177 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import toast from "react-hot-toast";
+import ReactQuill from "react-quill-new";
+import "../../../App.css";
+import ConfirmationModal from "../../modal/ConfirmationModal";
+import { getPlainContent } from "../../util/utilities";
+
+function AnswerEditor({ question, handleClose, saveEdit, editable }) {
+  const [editorContent, setEditorContent] = useState(question.review);
+  const [loading, setLoading] = useState(false);
+
+  const handleEditorChange = (content, delta, source, editor) => {
+    setEditorContent(content);
+  };
+
+  const getTextLength = () => {
+    return editorContent ? getPlainContent(editorContent).length : 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (getTextLength() === 0) {
+      return toast.error("후기 내용을 작성하세요!");
+    }
+    try {
+      setLoading(true);
+      const reviewData = { id: question.id, review: editorContent };
+
+      await saveEdit(reviewData);
+
+      toast.success("후기 저장 성공.");
+      handleClose();
+    } catch (err) {
+      console.error("err: ", err);
+      toast.error("후기 저장 오류!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Custom toolbar configuration
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ color: [] }, { background: [] }],
+      [{ align: [] }],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "list",
+    "indent",
+    "color",
+    "background",
+    "align",
+    "link",
+    "image",
+    "video",
+  ];
+
+  const [showModal, setShowModal] = useState(false);
+
+  const confirmDeletion = () => {
+    setShowModal(true);
+  };
+
+  const performDeletion = async () => {
+    try {
+      setLoading(true);
+      const reviewData = { id: question.id, review: null };
+
+      await saveEdit(reviewData);
+
+      toast.success("후기 삭제 완료");
+      handleClose();
+    } catch (err) {
+      console.error("err: ", err);
+      toast.error("후기 삭제 실패!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <ConfirmationModal
+        show={showModal}
+        handleClose={() => setShowModal(false)}
+        handleConfirm={performDeletion}
+        bodyMessage="후기를 삭제하려면, 삭제 버튼을 누르십시오!"
+        title="후기 삭제 확인"
+        noLabel="취소"
+        yesLabel="삭제"
+        headerBgColor="bg-danger"
+        modelClassName="modal-slide-down"
+      />
+      <Form className="mt-3 ms-3 mb-3" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 me-3" controlId="bumAnswer">
+          <Form.Label>
+            <h5 style={{ textAlign: "left" }} className="mb-2">
+              범이 답변
+            </h5>
+          </Form.Label>
+          <ReactQuill
+            theme="snow"
+            value={editorContent}
+            onChange={handleEditorChange}
+            modules={modules}
+            formats={formats}
+            placeholder="답변 작성 공간"
+            style={{
+              marginBottom: "10px",
+              borderRadius: "4px",
+            }}
+            className="ms-3 me-2"
+          />
+        </Form.Group>
+        <div className="d-flex gap-2 justify-content-center ">
+          <Button
+            variant="secondary"
+            type="button"
+            className="px-4"
+            onClick={() => handleClose()}
+          >
+            닫기
+          </Button>
+          {editable && (
+            <>
+              <Button
+                variant="primary"
+                type="submit"
+                className="px-4"
+                style={{ cursor: "pointer" }}
+                disabled={loading}
+              >
+                {loading ? <span>저장 중...</span> : "저장"}
+              </Button>
+              <Button
+                variant="outline-secondary"
+                type="button"
+                className="px-3"
+                onClick={() => setEditorContent("")}
+              >
+                초기화
+              </Button>
+            </>
+          )}
+          {question.question && editable && (
+            <Button
+              variant="danger"
+              type="button"
+              className="px-4"
+              disabled={loading}
+              onClick={confirmDeletion}
+            >
+              삭제
+            </Button>
+          )}
+        </div>
+      </Form>
+    </>
+  );
+}
+
+export default AnswerEditor;
