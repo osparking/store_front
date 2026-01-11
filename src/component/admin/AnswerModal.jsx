f081fce3f0a4aacccd01901d674a4899222c2ed8
BumTiger <jbpark03@gmail.com>
2026-01-11 21:18:34 +0900

    답변 모달 - 답변 편집기 및 질문 뷰어 포함

---
+++ src/component/admin/AnswerModal.jsx
@@ -0,0 +1,56 @@
+import { Form, Modal } from "react-bootstrap";
+import AnswerEditor from "../user/question/AnswerEditor";
+import QuestionViewer from "../user/question/QuestionViewer";
+
+export default function AnswerModal({
+  show,
+  handleClose,
+  question,
+  saveAnswer,
+}) {
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+      size="xl"
+      dialogClassName="quill-editor-modal"
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>답변 작성</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ paddingTop: 0 }}>
+        <AnswerEditor
+          question={question}
+          handleClose={handleClose}
+          saveEdit={saveAnswer}
+          editable={true}
+        />
+        <Form className="mt-3 ms-3 mb-3">
+          <Form.Label>
+            <h5 style={{ textAlign: "left" }}>고객 질문</h5>
+          </Form.Label>
+          <Form.Group className="mb-3 ms-3 me-4">
+            <Form.Group className="mb-0" controlId="formBasicEmail">
+              <Form.Label className="mt-1">제목</Form.Label>
+              <Form.Control
+                type="text"
+                maxLength={40}
+                name="title"
+                value={question.title}
+                className="mb-3"
+                style={{ fontSize: "13px" }}
+                readOnly={"true"}
+              />
+            </Form.Group>
+            <Form.Group className="mb-0" controlId="formBasicEmail">
+              <Form.Label>내용</Form.Label>
+              <QuestionViewer question={question.question} />
+            </Form.Group>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+}
