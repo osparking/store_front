364ac787aa3fb7d6d1fc07cca1b294472cd4133b
BumTiger <jbpark03@gmail.com>
2026-01-15 15:53:00 +0900

    질문 뷰어를 수정하여 QuillViewer 사용함

--- src/component/user/question/QuestionViewer.jsx
+++ src/component/user/question/QuestionViewer.jsx
@@ -1,20 +1,35 @@
-import ReactQuill from "react-quill-new";
+import React from "react";
+import { Form } from "react-bootstrap";
+import QuillViewer from "./QuillViewer";
 
-const QuestionViewer = ({ question }) => {
-  const modulesNone = {
-    toolbar: false, // No toolbar at all
-  };
-
+const QuestionViewer = ({ question, mine }) => {
+  console.log("question viewer");
   return (
-    <ReactQuill
-      modules={modulesNone}
-      value={question}
-      readOnly={true}
-      style={{
-        marginBottom: "10px",
-        borderRadius: "4px",
-      }}
-    />
+    <Form className="mt-3 ms-3 mb-3">
+      <Form.Label>
+        <h5 style={{ textAlign: "left" }}>
+          {mine ? "나의 질문" : "고객 질문"}
+        </h5>
+      </Form.Label>
+      <Form.Group className="mb-3 ms-3 me-4">
+        <Form.Group className="mb-0" controlId="formBasicEmail">
+          <Form.Label className="mt-1">제목</Form.Label>
+          <Form.Control
+            type="text"
+            maxLength={40}
+            name="title"
+            value={question.title}
+            className="mb-3"
+            style={{ fontSize: "13px" }}
+            readOnly={true}
+          />
+        </Form.Group>
+        <Form.Group className="mb-0" controlId="formBasicEmail">
+          <Form.Label>내용</Form.Label>
+          <QuillViewer question={question.question} />
+        </Form.Group>
+      </Form.Group>
+    </Form>
   );
 };
 
