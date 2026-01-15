c1737fb71310cc9285ff4c63e9f724a7355c5a5e
BumTiger <jbpark03@gmail.com>
2026-01-15 15:51:01 +0900

    질문 뷰어/편집기 중 택1, 닫기버튼 추가

--- src/component/admin/QuestionFollowUpModal.jsx
+++ src/component/admin/QuestionFollowUpModal.jsx
@@ -1,8 +1,9 @@
-import { Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import FollowUpEditor from "../user/question/FollowUpEditor";
 import QuestionViewer from "../user/question/QuestionViewer";
 import "./QuestionFollowUpModal.css";
 import FollowUpViewer from "../user/question/FollowUpViewer";
+import QuestionEditor from "../user/question/QuestionEditor";
 
 export default function QuestionFollowUpModal({
   show,
@@ -39,29 +40,35 @@ export default function QuestionFollowUpModal({
             />
           )
         )}
-        <Form className="mt-3 ms-3 mb-3">
-          <Form.Label>
-            <h5 style={{ textAlign: "left" }}>고객 질문</h5>
-          </Form.Label>
-          <Form.Group className="mb-3 ms-3 me-4">
-            <Form.Group className="mb-0" controlId="formBasicEmail">
-              <Form.Label className="mt-1">제목</Form.Label>
-              <Form.Control
-                type="text"
-                maxLength={40}
-                name="title"
-                value={question.title}
-                className="mb-3"
-                style={{ fontSize: "13px" }}
-                readOnly={"true"}
-              />
-            </Form.Group>
-            <Form.Group className="mb-0" controlId="formBasicEmail">
-              <Form.Label>내용</Form.Label>
-              <QuestionViewer question={question.question} />
-            </Form.Group>
-          </Form.Group>
-        </Form>
+        <div className="mt-5">
+          {(question.answered || is_admin) ? (
+            <QuestionViewer question={question} mine={mine} />
+          ) : (
+            <QuestionEditor
+              question={question}
+              mine={mine}
+              handleClose={handleClose}
+            />
+          )}
+        </div>
+
+        {question.answered && mine && (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="secondary"
+              className="w-60 mt-4 mb-4 px-4"
+              onClick={handleClose}
+            >
+              닫기
+            </Button>
+          </div>
+        )}        
       </Modal.Body>
     </Modal>
   );
