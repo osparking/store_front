61e920fd9882c609a4df25c7e03f88652d852f24
BumTiger <jbpark03@gmail.com>
2026-01-19 20:55:38 +0900

    헤드 텍스트 프롭 접수 - 헤더에 사용

--- src/component/user/question/FollowUpViewer.jsx
+++ src/component/user/question/FollowUpViewer.jsx
@@ -3,17 +3,16 @@ import { Form } from "react-bootstrap";
 import "../../../App.css";
 import QuillViewer from "./QuillViewer";
 
-function FollowUpViewer({ followUp }) {
+function FollowUpViewer({ followUp, headText }) {
   console.log("followUp viewer");
 
   return (
     <Form className="mt-3 ms-3 mb-3">
       <Form.Label>
-        <h5 style={{ textAlign: "left" }}>답변</h5>
+        <h5 style={{ textAlign: "left" }}>{headText}</h5>
       </Form.Label>
       <Form.Group className="mb-3 ms-3 me-4">
         <Form.Group className="mb-0" controlId="formBasicEmail">
-          <Form.Label>내용</Form.Label>
           <QuillViewer question={followUp.content} />
         </Form.Group>
       </Form.Group>
