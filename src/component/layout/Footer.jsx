d47fe1d5ea91500f997c57e94ca256cf361c4783
BumTiger <jbpark03@gmail.com>
2026-01-24 14:13:46 +0900

    푸터 성분 및 묘양새

---
+++ src/component/layout/Footer.jsx
@@ -0,0 +1,30 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="text-white bottomBar mt-auto me-0">
+      <Container fluid className="px-5 ">
+        <Row>
+          <Col md={1}>
+            <h6></h6>
+          </Col>
+          <Col md={4}>
+            <h6>범이비누</h6>
+            <p>© {new Date().getFullYear()} All rights reserved.</p>
+          </Col>
+          <Col md={4}>
+            <h6>이메일</h6>
+            <p>jbpark03@gmail.com</p>
+          </Col>
+          <Col md={3}>
+            <h6>휴대폰</h6>
+            <p>(010) 2899-9168</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
