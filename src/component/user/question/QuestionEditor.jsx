9b73c6590a820365a1879a6ae808434e80209e0a
BumTiger <jbpark03@gmail.com>
2026-01-27 11:57:22 +0900

    질문 편집기의 최 외곽 div를 Container-Card 로 두른다

--- src/component/user/question/QuestionEditor.jsx
+++ src/component/user/question/QuestionEditor.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useRef, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
 import toast from "react-hot-toast";
 import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css"; // Import styles
@@ -154,75 +154,89 @@ function QuestionEditor({ question, mine, handleClose, setReloadPage }) {
   }, []);
 
   return (
-    <div className="d-flex justify-content-center align-items-center vh-67">
-      <Form
-        onSubmit={handleSubmit}
-        className={mine ? "question-editor-modal" : "question-editor-window"}
-      >
-        <Form.Group className="mb-3">
-          <Form.Label>
-            <h5 style={{ textAlign: "left" }}>
-              {mine ? "나의 질문" : "고객 질문"}
-            </h5>
-          </Form.Label>
-          <Form.Group className="mb-0" controlId="formBasicEmail">
-            <Form.Label className="mt-3">제목</Form.Label>
-            <Form.Control
-              type="text"
-              maxLength={40}
-              name="title"
-              defaultValue={question ? question.title : ""}
-              placeholder="(제목 입력)"
-            />
-            <Form.Text className="text-muted ms-2">
-              (내용을 30 자 내외로 요약하세요.)
-            </Form.Text>
-          </Form.Group>
-          <Form.Label className="mt-3">내용</Form.Label>
-          <ReactQuill
-            ref={quillRef}
-            theme="snow"
-            value={editorContent || placeholder}
-            onChange={handleEditorChange}
-            onFocus={clearPlaceholder}
-            onBlur={handleEditorBlur}
-            modules={modules}
-            formats={formats}
-            className="content-edit"
-          />
-        </Form.Group>
+    <Container fluid className="home-container mt-5">
+      <Row className="justify-content-center mt-3">
+        <Col md={9} className="mt">
+          <Card>
+            <Card.Body>
+              <div className="d-flex justify-content-center align-items-center vh-67">
+                <Form
+                  onSubmit={handleSubmit}
+                  className={
+                    mine ? "question-editor-modal" : "question-editor-window"
+                  }
+                >
+                  <Form.Group className="mb-3">
+                    <Form.Label>
+                      <h5 style={{ textAlign: "left" }}>
+                        {mine ? "나의 질문" : "고객 질문"}
+                      </h5>
+                    </Form.Label>
+                    <Form.Group className="mb-0" controlId="formBasicEmail">
+                      <Form.Label className="mt-3">제목</Form.Label>
+                      <Form.Control
+                        type="text"
+                        maxLength={40}
+                        name="title"
+                        defaultValue={question ? question.title : ""}
+                        placeholder="(제목 입력)"
+                      />
+                      <Form.Text className="text-muted ms-2">
+                        (내용을 30 자 내외로 요약하세요.)
+                      </Form.Text>
+                    </Form.Group>
+                    <Form.Label className="mt-3">내용</Form.Label>
+                    <ReactQuill
+                      ref={quillRef}
+                      theme="snow"
+                      value={editorContent || placeholder}
+                      onChange={handleEditorChange}
+                      onFocus={clearPlaceholder}
+                      onBlur={handleEditorBlur}
+                      modules={modules}
+                      formats={formats}
+                      className="content-edit"
+                    />
+                  </Form.Group>
 
-        <div className="text-muted mb-3">글자수: {getTextLength()} 자</div>
+                  <div className="text-muted mb-3">
+                    글자수: {getTextLength()} 자
+                  </div>
 
-        <div className="d-flex gap-2 justify-content-center mb-3">
-          <Button
-            variant="secondary"
-            type="button"
-            className="px-4"
-            onClick={() => closeEditor()}
-          >
-            닫기
-          </Button>
-          <Button
-            variant="primary"
-            type="submit"
-            className="px-4"
-            style={{ cursor: "pointer" }}
-            disabled={saving}
-          >
-            {saving ? <span>저장 중...</span> : "저장"}
-          </Button>
-          <Button
-            variant="outline-secondary"
-            type="button"
-            className="px-3"
-            onClick={() => setEditorContent("")}
-          >
-            초기화
-          </Button>
-        </div>
-      </Form>
-    </div>
+                  <div className="d-flex gap-2 justify-content-center mb-3">
+                    <Button
+                      variant="secondary"
+                      type="button"
+                      className="px-4"
+                      onClick={() => closeEditor()}
+                    >
+                      닫기
+                    </Button>
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      className="px-4"
+                      style={{ cursor: "pointer" }}
+                      disabled={saving}
+                    >
+                      {saving ? <span>저장 중...</span> : "저장"}
+                    </Button>
+                    <Button
+                      variant="outline-secondary"
+                      type="button"
+                      className="px-3"
+                      onClick={() => setEditorContent("")}
+                    >
+                      초기화
+                    </Button>
+                  </div>
+                </Form>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
