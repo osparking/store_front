89855abba2c5ef8ef23abf614eeeb2117bd253b9
BumTiger <jbpark03@gmail.com>
2026-01-28 11:45:00 +0900

    비누 재료 성분 카드 최대 높이 지정 등

--- src/component/soaps/Ingredient.jsx
+++ src/component/soaps/Ingredient.jsx
@@ -1,33 +1,27 @@
 import { Card, Col, Container, Row } from "react-bootstrap";
 import "../../index.css";
 import { ingAllData } from "./ingAllData";
-import "./Ingredient.css";
+import "./ingredient.css";
 import IngredTabAccord from "./IngredTabAccord";
-
-const ingAllRows = ingAllData.map((ingred, idx) => (
-  <tr className={ingred.id === "" ? "table-row" : ""} key={idx}>
-    <td>{ingred.id}</td>
-    <td>{ingred.name}</td>
-    <td className="text-center">{ingred.weight}</td>
-    <td className="text-center">{ingred.incRate}</td>
-    <td>{ingred.etcEffect}</td>
-  </tr>
-));
 
 const Ingredient = () => {
   return (
     <Container fluid className="home-container mt-5">
       <Row className="justify-content-center allIngred mt-3">
-        <Col md={9} className="mt">
-          <Card>
+        <Col md={8} className="mt">
+          <Card style={{ maxHeight: "720px" }}>
             <Card.Body className="pb-0">
-              <h2 className="mb-1" style={{ paddingLeft: 0 }}>
-                <strong>재료 함량</strong>
-                <br />
-                <small>(비누 1 개용 원액 126g 기준)</small>
-              </h2>
-              <hr />
-              <IngredTabAccord ingAllData={ingAllData} keepOthersOpen={true} />
+              <div className="mt-3 mb-5">
+                <h2 className="mb-1" style={{ paddingLeft: 0 }}>
+                  <strong>재료 함량</strong>
+                  <br />
+                </h2>
+                <p className="mb-0 text-end legendFont">(비누 1 개용 원액 126.3g 기준)</p>
+                <IngredTabAccord
+                  ingAllData={ingAllData}
+                  keepOthersOpen={false}
+                />
+              </div>
             </Card.Body>
           </Card>
         </Col>
