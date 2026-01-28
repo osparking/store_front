1414ea77fba2ab92b846216c09854a9eb70484aa
BumTiger <jbpark03@gmail.com>
2026-01-29 07:27:23 +0900

    비누 외형 탭 내용 중 두 헤딩 모양 조정

--- src/component/soaps/BumShapes.jsx
+++ src/component/soaps/BumShapes.jsx
@@ -34,11 +34,11 @@ const BumShapes = () => {
         <Col md={8}>
           <Card>
             <Card.Body>
-              <h2 className="ps-0 m-4">
-                <strong>비누 외형 3 종</strong>
+              <h2 className="mb-1">
+                <strong>비누 외형</strong>
               </h2>
-              <h5>
-                범이비누의 세가지 외형은 다음 두 요소에 의하여 빚어집니다.
+              <h5 className="m-3 tabBackground pt-1 pb-1">
+                다음 두 요소가 범이비누의 외형을 정합니다.
               </h5>
               <ul>
                 <li>
