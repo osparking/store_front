9764abd56a3d9d2d294e8f98d3865d1f059e8a4b
BumTiger <jbpark03@gmail.com>
2026-01-29 09:59:40 +0900

    비누 재료 탭 내용 골격 모양새 수정

--- src/component/soaps/Ingredient.jsx
+++ src/component/soaps/Ingredient.jsx
@@ -8,15 +8,17 @@ const Ingredient = () => {
   return (
     <Container fluid className="home-container mt-5">
       <Row className="justify-content-center allIngred mt-3">
-        <Col md={8} className="mt">
-          <Card style={{ maxHeight: "720px" }}>
+        <Col md={8}>
+          <Card className="cardIngredient">
             <Card.Body className="pb-0">
-              <div className="mt-3 mb-5">
-                <h2 className="mb-1" style={{ paddingLeft: 0 }}>
+              <div className="mb-5">
+                <h2 className="mb-3" style={{ paddingLeft: 0 }}>
                   <strong>재료 함량</strong>
                   <br />
                 </h2>
-                <p className="mb-0 text-end legendFont">(비누 1 개용 원액 126.3g 기준)</p>
+                <p className="mb-0 text-end legendFont">
+                  (비누 1 개용 원액 126.3g 기준)
+                </p>
                 <IngredTabAccord
                   ingAllData={ingAllData}
                   keepOthersOpen={false}
