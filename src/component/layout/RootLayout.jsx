ae2b9661c8f7c5493e5162367dee2ec8d24f6d1e
BumTiger <jbpark03@gmail.com>
2026-01-31 17:59:50 +0900

    홈페이지 주요 내용 위한 유일한 div 에 클래스 지정

--- src/component/layout/RootLayout.jsx
+++ src/component/layout/RootLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import BackgroundImageSlider from "../common/BackgroundImageSlider";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import "./RootLayout.css";
 
 const RootLayout = () => {
   return (
@@ -10,7 +11,7 @@ const RootLayout = () => {
       <NavBar />
       <Toaster position="bottom-center" reverseOrder={false} />
       <BackgroundImageSlider />
-      <div>
+      <div className="main-content">
         <Outlet />
       </div>
       <Footer />
