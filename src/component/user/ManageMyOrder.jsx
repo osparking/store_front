fa34cd6b7d0372aabe20df4eef8ec09eaac711e3
BumTiger <jbpark03@gmail.com>
2026-02-03 14:47:13 +0900

    나의 주문 테이블을 카드 내에 위치함

--- src/component/user/ManageMyOrder.jsx
+++ src/component/user/ManageMyOrder.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import "../../App.css";
 import MyOrdersPage from "../pay_toss/MyOrdersPage";
 import OrderDetail from "../worker/order/OrderDetail";
-import { Container } from "react-bootstrap";
+import "./ManageMyOrder.css";
 
 const ManageMyOrder = () => {
   const [showDetail, setShowDetail] = useState(false);
   const [detailId, setDetailId] = useState(0);
 
   return (
-    <Container fluid className="home-container mt-3">
-      <h5 className="chart-title">주문 관리</h5>
-      {showDetail ? (
-        <OrderDetail
-          detailId={detailId}
-          setShowDetail={setShowDetail}
-          isHouse={false}
-        />
-      ) : (
-        <MyOrdersPage setShowDetail={setShowDetail} setDetailId={setDetailId} />
-      )}
+    <Container fluid className="home-container mt-5">
+      <Row className="justify-content-center mt-3">
+        <Col>
+          <Card>
+            <Card.Body>
+              <div>
+                <h2 className="mb-1 ps-0">
+                  <strong>나의 주문 목록</strong>
+                </h2>
+                {showDetail ? (
+                  <OrderDetail
+                    detailId={detailId}
+                    setShowDetail={setShowDetail}
+                    isHouse={false}
+                  />
+                ) : (
+                  <MyOrdersPage
+                    setShowDetail={setShowDetail}
+                    setDetailId={setDetailId}
+                  />
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
