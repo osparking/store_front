f7d33d5a1319967982e90768b856f526b5971e45
BumTiger <jbpark03@gmail.com>
2026-02-04 14:46:18 +0900

    독립 성분화된 나의 리뷰 행 페이지 테이블

---
+++ src/component/user/review/ReviewsTable.jsx
@@ -0,0 +1,38 @@
+import { Table } from "react-bootstrap";
+import { formatDate } from "../../util/utilities";
+
+const ReviewsTable = (reviews, manageReview) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>주문명</th>
+          <th>주문일시</th>
+          <th>별점</th>
+          <th>후기 시작 부분</th>
+          <th>후기 작성일시</th>
+          <th>주문ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        {reviews &&
+          reviews.map((review, idx) => (
+            <tr key={idx}>
+              <td>{review.orderName}</td>
+              <td>{formatDate(review.orderTime)}</td>
+              <td>{review.stars}</td>
+              <td className="text-start">
+                <a href="#" onClick={() => manageReview(review)}>
+                  {review.reviewPreview}
+                </a>
+              </td>
+              <td>{formatDate(review.reviewTime)}</td>
+              <td className="text-center">{review.id}</td>
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default ReviewsTable;
