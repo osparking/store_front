ba13e57fab2daf4009b4699e503c4203fb77dfa5
BumTiger <jbpark03@gmail.com>
2026-02-04 19:46:08 +0900

    나의 리뷰 탭 내용 상단 마진 증가 3 > 5

--- src/component/user/review/ManageMyReviews.jsx
+++ src/component/user/review/ManageMyReviews.jsx
@@ -9,8 +9,7 @@ const ManageMyReviews = () => {
   const [detailId, setDetailId] = useState(0);
 
   return (
-    <Container fluid className="home-container mt-3">
-      <h5 className="chart-title">후기 관리</h5>
+    <Container fluid className="home-container mt-5">
       {showDetail ? (
         <OrderDetail
           detailId={detailId}
