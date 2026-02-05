161a00738262c6bb89a50ca380abdf22a60eeb05
BumTiger <jbpark03@gmail.com>
2026-02-05 12:11:38 +0900

    callWithToken 함수 인식 401, 403 오류 처리

--- src/component/util/api.js
+++ src/component/util/api.js
@@ -1,6 +1,6 @@
 import axios, { HttpStatusCode } from "axios";
+import toast from "react-hot-toast";
 import { expiredTokenRemoved } from "./utilities";
-import { Navigate, useNavigate } from "react-router-dom";
 
 const prefix = "http://localhost:9193/api/s1";
 
@@ -14,7 +14,6 @@ export const apic = axios.create({
 });
 
 export async function callWithToken(method, urlSuffix, data = null) {
-  
   try {
     const token = localStorage.getItem("TOKEN");
     if (token) {
@@ -55,6 +54,7 @@ export async function callWithToken(method, urlSuffix, data = null) {
       err.response.status === HttpStatusCode.Forbidden ||
       err.response.status === HttpStatusCode.Unauthorized
     ) {
+      toast.error(err?.response?.data.message);
       return null;
     } else {
       throw err;
