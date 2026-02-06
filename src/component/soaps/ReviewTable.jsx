3de73fdf5b778df1a7e54197ee0cc0eb6eb0ec3c
BumTiger <jbpark03@gmail.com>
2026-02-06 14:30:38 +0900

    구매후기 성분 반환값 서두 부분 Container 사용

--- src/component/soaps/ReviewTable.jsx
+++ src/component/soaps/ReviewTable.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Card, Col, Container, Row, Table } from "react-bootstrap";
 import "../../App.css";
 import {
   fetchAverageStars,
@@ -64,7 +64,7 @@ const ReviewTable = () => {
   const [review, setReview] = useState({});
 
   return (
-    <div className="box_section review_table_div" style={{ overflowX: "auto" }}>
+    <Container fluid className="home-container mt-5">
       <ReviewModal
         show={showReviewModal}
         handleClose={() => setShowReviewModal(false)}
@@ -72,64 +72,78 @@ const ReviewTable = () => {
         review={review}
         editable={false}
       />
-      <h5 className="chart-title pinkBack">고객 구매 후기</h5>
-      <p className="text-center mb-0 mt-4">
-        <RatingAvg rating={averageStars} />({averageStars.toFixed(2)} — 총{" "}
-        {reviewPage.totalElements}건 평균)
-      </p>
+      {/* <h5 className="chart-title pinkBack">고객 구매 후기</h5> */}
+      <Row className="justify-content-center mt-3">
+        <Col id="reviewTable">
+          <Card>
+            <Card.Body>
+              <h2 className="mb-1 ps-0">
+                <strong>고객 구매 후기</strong>
+              </h2>
+              <div className="justify-content-center align-items-center">
+                <p className="text-center text-muted mb-4">
+                  <RatingAvg rating={averageStars} />({averageStars.toFixed(2)}{" "}
+                  — 총 {reviewPage.totalElements}건 평균)
+                </p>
+              </div>
 
-      <div className="table-container">
-        <Table bordered hover striped>
-          <thead>
-            <tr>
-              <th>주문 일시</th>
-              <th>후기 작성</th>
-              <th>별점</th>
-              <th>후기 시작 부분</th>
-              <th>비누 외형</th>
-              <th>사진</th>
-              <th>유튜브</th>
-            </tr>
-          </thead>
-          <tbody>
-            {reviews &&
-              reviews.map((review, index) => (
-                <tr key={index}>
-                  <td>{review.orderTimeStr}</td>
-                  <td>{review.reviewDayDelay}일 후</td>
-                  <td>{review.stars}</td>
-                  <td
-                    style={{ cursor: "pointer" }}
-                    className="text-start linkLook text-primary"
-                    onClick={() =>
-                      viewReviewDetail(
-                        review.id,
-                        review.customerName,
-                        review.reviewTime
-                      )
-                    }
-                  >
-                    {review.reviewPreview}
-                  </td>
-                  <td>{review.shapesList}</td>
-                  <td>{review.hasImage ? "있음" : ""}</td>
-                  <td>{review.hasVideo ? "있음" : ""}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-      </div>
-      {fetchResult && reviewPage && (
-        <Paginator
-          pageSize={reviewPage.size}
-          totalItems={reviewPage.totalElements}
-          totalPages={totalPages}
-          currPage={fetchResult.currentPage}
-          setCurrPage={(pageNo) => setCurrentPage(pageNo)}
-          darkBackground="true"
-        />
-      )}
-    </div>
+              <div className="table-container">
+                <Table bordered hover striped>
+                  <thead>
+                    <tr>
+                      <th className="minDateWidth">주문 일시</th>
+                      <th>작성 시점</th>
+                      <th>별점</th>
+                      <th>후기 시작 부분</th>
+                      <th>비누 외형</th>
+                      <th>사진</th>
+                      <th>유튜브</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {reviews &&
+                      reviews.map((review, index) => (
+                        <tr key={index}>
+                          <td>{review.orderTimeStr}</td>
+                          <td>{review.reviewDayDelay}일 후</td>
+                          <td>{review.stars}</td>
+                          <td
+                            style={{ cursor: "pointer" }}
+                            className="text-start linkLook text-primary"
+                            onClick={() =>
+                              viewReviewDetail(
+                                review.id,
+                                review.customerName,
+                                review.reviewTime,
+                              )
+                            }
+                          >
+                            {review.reviewPreview}
+                          </td>
+                          <td>{review.shapesList}</td>
+                          <td>{review.hasImage ? "있음" : ""}</td>
+                          <td>{review.hasVideo ? "있음" : ""}</td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </Table>
+              </div>
+              {fetchResult && reviewPage && (
+                <Paginator
+                  pageSize={reviewPage.size}
+                  totalItems={reviewPage.totalElements}
+                  totalPages={totalPages}
+                  currPage={fetchResult.currentPage}
+                  setCurrPage={(pageNo) => setCurrentPage(pageNo)}
+                  darkBackground="true"
+                />
+              )}
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <p className="text-center mb-0 mt-4"></p>
+    </Container>
   );
 };
 
