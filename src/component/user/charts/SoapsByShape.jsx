1e28ac7a3aac3a5032c2eb76e62e577938a27f01
BumTiger <jbpark03@gmail.com>
2026-02-10 12:01:10 +0900

    월별 수량과 비슷하게 외형별 수량도 개정

---
+++ src/component/user/charts/SoapsByShape.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import NoDataExists from "../../common/NoDataExists";
+import useColorMapping from "../../hook/ColorMapping";
+import { getShapeCount } from "../../user/UserService";
+
+const SoapsByShape = ({ totalSoaps }) => {
+  const [shapeCount, setShapeCount] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const getSoapShapeStat = async () => {
+      try {
+        const userId = localStorage.getItem("LOGIN_ID");
+        const result = await getShapeCount(userId);
+        setShapeCount(result.data);
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    };
+    getSoapShapeStat();
+  }, []);
+
+  const colors = useColorMapping();
+
+  return (
+    <>
+      <h5
+        className="chart-title mb-1 p-1"
+        style={{ backgroundColor: "hsl(83, 26%, 50%)", color: "ivory" }}
+      >
+        비누 외형 비중
+      </h5>
+      {shapeCount && (
+        <ResponsiveContainer
+          className="userChart"
+          style={{ backgroundColor: "#d8aab8ff" }}
+        >
+          {totalSoaps === 0 ? (
+            <NoDataExists dataType={""} />
+          ) : (
+            <PieChart className="p-3" style={{ backgroundColor: "#d8aab8ff" }}>
+              <Pie
+                data={shapeCount}
+                dataKey="count"
+                nameKey="shapeLabel"
+                label={({ shapeLabel, count, percent }) =>
+                  `${shapeLabel}: ${Math.trunc(count)} (${(
+                    percent * 100
+                  ).toFixed(1)}%)`
+                }
+                outerRadius={45}
+                fill="#8884d8"
+              >
+                {shapeCount.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={colors[entry.shapeLabel]} />
+                ))}
+              </Pie>
+              <Tooltip
+                formatter={(value, name) => [`${Math.trunc(value)}개`, name]}
+              />
+              <Legend layout="horizontal" />
+            </PieChart>
+          )}
+        </ResponsiveContainer>
+      )}
+    </>
+  );
+};
+
+export default SoapsByShape;
