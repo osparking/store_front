cba06c8872e35cd7aab383d4286d299ce08a006c
BumTiger <jbpark03@gmail.com>
2026-02-11 11:43:24 +0900

    프로필 비밀번호 링크 수평 중앙 배치

--- src/component/user/UserProfile.jsx
+++ src/component/user/UserProfile.jsx
@@ -106,7 +106,7 @@ const UserProfile = ({ user, handleRemovePhoto }) => {
           >
             <Card className="text-center mb-3 shadow">
               <Card.Body>
-                <div>
+                <div className="d-flex justify-content-center">
                   {!(user.userType === "고객") && (
                     <>
                       <EmpImage empPhoto={user.photoBytes} />
