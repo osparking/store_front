8f759fea414b6419e98587a6bda8d4b6c5ae703e
BumTiger <jbpark03@gmail.com>
2026-02-16 08:39:46 +0900

    결제(첵크아웃) 페이지 주문 요약 div 모양새 조절

--- src/component/buy/OrderDigest.jsx
+++ src/component/buy/OrderDigest.jsx
@@ -1,18 +1,16 @@
 import { Col, Row } from "react-bootstrap";
 import "./OrderDigest.css";
 
-const OrderDigest = ({ name, amount, address}) => {
+const OrderDigest = ({ name, amount, address }) => {
   return (
     <Row className="d-flex justify-content-center">
       <Col xs={12} md={12}>
         {name && (
-          <div
-            md={6}
-            className="small-font rounded"
-            style={{ backgroundColor: "ivory", fontSize: "13px" }}
-          >
-            <h6>주문 요약</h6>
-            <ul className="ms-4">
+          <div md={6} className="rounded">
+            <h5 className="orderDigest">주문 요약</h5>
+            <ul className="mb-0"
+              style={{ marginLeft: "120px", fontSize: "13px" }}
+            >
               <li>주문 이름 : {name}</li>
               <li className="mt_0">결제 금액 : {amount.toLocaleString()} 원</li>
               <li className="mt_0">주소 : {address}</li>
