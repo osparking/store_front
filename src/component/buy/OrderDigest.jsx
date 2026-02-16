d3d8573d71dd6fe2beaebae99c1733dbafb1a09a
BumTiger <jbpark03@gmail.com>
2026-02-16 09:44:12 +0900

    주문 요약 ul 스타일 제거 등 단순화

--- src/component/buy/OrderDigest.jsx
+++ src/component/buy/OrderDigest.jsx
@@ -8,12 +8,10 @@ const OrderDigest = ({ name, amount, address }) => {
         {name && (
           <div md={6} className="rounded">
             <h5 className="orderDigest">주문 요약</h5>
-            <ul className="mb-0"
-              style={{ marginLeft: "120px", fontSize: "13px" }}
-            >
+            <ul className="mb-0">
               <li>주문 이름 : {name}</li>
-              <li className="mt_0">결제 금액 : {amount.toLocaleString()} 원</li>
-              <li className="mt_0">주소 : {address}</li>
+              <li className="mt-0">결제 금액 : {amount.toLocaleString()} 원</li>
+              <li className="mt-0">주소 : {address}</li>
             </ul>
           </div>
         )}
