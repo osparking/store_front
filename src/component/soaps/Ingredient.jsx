40b565789ec3c94fd936c15ecf1761aa997eebc1
BumTiger <jbpark03@gmail.com>
2026-02-22 12:30:54 +0900

    비누재료 탭 쪽 - 이중 수직 스크롤바 제거 등

--- src/component/soaps/Ingredient.jsx
+++ src/component/soaps/Ingredient.jsx
@@ -6,29 +6,27 @@ import IngredTabAccord from "./IngredTabAccord";
 
 const Ingredient = () => {
   return (
-    <Container fluid className="home-container mt-5">
-      <Row className="justify-content-center mt-3">
-        <Col md={8}>
-          <Card>
-            <Card.Body className="pb-0">
-              <div className="mb-5">
-                <h2 className="mb-3" style={{ paddingLeft: 0 }}>
-                  <strong>재료 함량</strong>
-                  <br />
-                </h2>
-                <p className="mb-0 text-end legendFont">
-                  (비누 1 개용 원액 126.3g 기준)
-                </p>
-                <IngredTabAccord
-                  ingAllData={ingAllData}
-                  keepOthersOpen={false}
-                />
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <Row
+      className="justify-content-center mt-2 mb-2"
+      style={{ height: "80vh" }}
+    >
+      <Col md={8}>
+        <Card style={{ minWidth: "620px" }}>
+          <Card.Body className="pb-0">
+            <div className="mb-5">
+              <h2 className="mb-3" style={{ paddingLeft: 0 }}>
+                <strong>재료 함량</strong>
+                <br />
+              </h2>
+              <p className="mb-0 text-end legendFont">
+                (비누 1 개용 원액 126.3g 기준)
+              </p>
+              <IngredTabAccord ingAllData={ingAllData} keepOthersOpen={false} />
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
