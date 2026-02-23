be390e4e9ae1b282dbd1678c0bc51d0c214083d5
BumTiger <jbpark03@gmail.com>
2026-02-23 14:27:46 +0900

    챠트2, width, height 경고 해소

--- src/component/user/charts/SoapsByMonth.jsx
+++ src/component/user/charts/SoapsByMonth.jsx
@@ -1,13 +1,12 @@
 import {
-    Bar,
-    BarChart,
-    CartesianGrid,
-    Cell,
-    Legend,
-    ResponsiveContainer,
-    Tooltip,
-    XAxis,
-    YAxis,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
 } from "recharts";
 import NoDataExists from "../../common/NoDataExists";
 
@@ -23,32 +22,35 @@ const SoapsByMonth = ({ totalSoaps, soapsMonth }) => {
       >
         비누 구매 수량
       </h5>
-      <ResponsiveContainer className="userChart">
-        {totalSoaps > 0 ? (
-          <BarChart data={soapsMonth} style={{ backgroundColor: "white" }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="month" angle={-30} textAnchor="end" height={70} />
-            <YAxis
-              width={40}
-              tick={{ fill: "#232a31ff", fontSize: 15 }}
-              axisLine={{ stroke: "#495057" }}
-            />
-            <Tooltip />
-            <Bar dataKey="월별 수량">
-              {soapsMonth.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={entry.color} />
-              ))}
-            </Bar>
-            <Legend
-              formatter={(value, entry) => (
-                <span style={{ color: "#87a15e" }}>{value}</span>
-              )}
-            />
-          </BarChart>
-        ) : (
-          <NoDataExists dataType={""} />
-        )}
-      </ResponsiveContainer>
+      {totalSoaps > 0 ? (
+        <BarChart
+          width={328}
+          height={238}
+          data={soapsMonth}
+          style={{ backgroundColor: "white" }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" angle={-30} textAnchor="end" height={70} />
+          <YAxis
+            width={40}
+            tick={{ fill: "#232a31ff", fontSize: 15 }}
+            axisLine={{ stroke: "#495057" }}
+          />
+          <Tooltip />
+          <Bar dataKey="월별 수량">
+            {soapsMonth.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Bar>
+          <Legend
+            formatter={(value, entry) => (
+              <span style={{ color: "#87a15e" }}>{value}</span>
+            )}
+          />
+        </BarChart>
+      ) : (
+        <NoDataExists dataType={""} />
+      )}
     </>
   );
 };
