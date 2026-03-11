894291a24f894faa51966c57eb7e15ca9d9c1633
BumTiger <jbpark03@gmail.com>
2026-03-11 15:12:12 +0900

    만료토큰 제거함수가 바른 토큰 채취함

--- src/component/util/utilities.js
+++ src/component/util/utilities.js
@@ -140,7 +140,7 @@ export function getPlainContent(htmlContent) {
 }
 
 export const expiredTokenRemoved = () => {
-  const token = localStorage.getItem("TOKEN");
+  const token = getStorageToken();
 
   if (token) {
     try {
