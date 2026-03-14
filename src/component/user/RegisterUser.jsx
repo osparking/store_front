7b958bbe927104a2e671f3f61c4e6b7b699fd1e2
BumTiger <jbpark03@gmail.com>
2026-03-14 09:30:36 +0900

    Row, Col 제거, 새 클래스 사용, 이메일 하단 마진 감소

--- src/component/user/RegisterUser.jsx
+++ src/component/user/RegisterUser.jsx
@@ -7,6 +7,7 @@ import ProcessSpinner from "../common/ProcessSpinner";
 import BsAlertHook from "../hook/BsAlertHook";
 import WorkerDeptSelector from "../worker/WorkerDeptSelector";
 import { registerUser } from "./UserService";
+import "./RegisterUser.css";
 
 const RegisterUser = () => {
   const loginId = localStorage.getItem("LOGIN_ID");
@@ -87,155 +88,137 @@ const RegisterUser = () => {
   const isAdmin = localStorage.getItem("IS_ADMIN") === "true";
 
   return (
-    <Container fluid className="home-container">
-      <Row className="justify-content-center" style={{ marginTop: "8px" }}>
-        <Col xs={12} md={8} lg={6}>
-          <Form onSubmit={handleSubmit}>
-            <Card className="shadow mb-5">
-              <Card.Header className="text-center">
-                <legend>사용자 등록</legend>
-              </Card.Header>
-              <Card.Body>
-                <Form.Group as={Row} controlId="full-name" className="mb-4">
-                  <Col xs={6} className="mb-2 mb-sm-0">
-                    <Form.Label>성명</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="fullName"
-                      placeholder="(성명)"
-                      value={user.fullName}
+    <Container className="d-flex justify-content-center">
+      <Form onSubmit={handleSubmit}>
+        <Card className="shadow mb-5 registerCard">
+          <Card.Header className="text-center">
+            <legend>사용자 등록</legend>
+          </Card.Header>
+          <Card.Body>
+            <Form.Group as={Row} controlId="full-name" className="mb-4">
+              <Col xs={6} className="mb-2 mb-sm-0">
+                <Form.Label>성명</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="fullName"
+                  placeholder="(성명)"
+                  value={user.fullName}
+                  onChange={handleChange}
+                  required
+                />
+              </Col>
+            </Form.Group>
+
+            <fieldset className="mb-4">
+              <Form.Label>연락처</Form.Label>
+              <Row>
+                <Col xs={6} className="mb-2 mb-sm-0">
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="(이메일)"
+                    value={user.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </Col>
+                <Col xs={6}>
+                  <Form.Control
+                    type="text"
+                    name="mbPhone"
+                    placeholder="(휴대폰 번호)"
+                    value={user.mbPhone}
+                    onChange={handleChange}
+                    required
+                  />
+                </Col>
+              </Row>
+            </fieldset>
+
+            <Form.Group as={Row} controlId="password" className="mb-4">
+              <Col>
+                <Form.Label>패스워드</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  required
+                  placeholder="(비밀번호)"
+                  value={user.password}
+                  onChange={handleChange}
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="user-type" className="mb-4">
+              <Col>
+                <Form.Label>계정 유형</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="userType"
+                  required
+                  value={user.userType}
+                  onChange={handleChange}
+                >
+                  <option value="">(계정 타입)</option>
+                  <option value="CUSTOMER">고객</option>
+                  <option value="WORKER">직원</option>
+                </Form.Control>
+              </Col>
+            </Form.Group>
+
+            {user.userType === "WORKER" && (
+              <Form.Group>
+                <Row>
+                  <Col>
+                    <WorkerDeptSelector
+                      workerDept={user.dept}
                       onChange={handleChange}
-                      required
                     />
                   </Col>
-                </Form.Group>
+                </Row>
+              </Form.Group>
+            )}
 
-                <fieldset className="mb-4">
-                  <Form.Label>연락처</Form.Label>
-                  <Row>
-                    <Col xs={6} className="mb-3 mb-sm-0">
-                      <Form.Control
-                        type="email"
-                        name="email"
-                        placeholder="(이메일)"
-                        value={user.email}
-                        onChange={handleChange}
-                        required
-                      />
-                    </Col>
-                    <Col xs={6}>
-                      <Form.Control
-                        type="text"
-                        name="mbPhone"
-                        placeholder="(휴대폰 번호)"
-                        value={user.mbPhone}
-                        onChange={handleChange}
-                        required
-                      />
-                    </Col>
-                  </Row>
-                </fieldset>
+            <div className="d-flex justify-content-center mb-3 mt-3 char2button">
+              <Button
+                type="submit"
+                variant="outline-primary"
+                size="sm"
+                className="me-2"
+                disabled={isProcessing}
+              >
+                {isProcessing ? <ProcessSpinner message="유저 등록" /> : "등록"}
+              </Button>
+              <Button variant="outline-info" size="sm" onClick={handleReset}>
+                리셋
+              </Button>
+            </div>
 
-                <Form.Group as={Row} controlId="password" className="mb-4">
-                  <Col>
-                    <Form.Label>패스워드</Form.Label>
-                    <Form.Control
-                      type="password"
-                      name="password"
-                      required
-                      placeholder="(비밀번호)"
-                      value={user.password}
-                      onChange={handleChange}
-                    />
-                  </Col>
-                </Form.Group>
-
-                <Form.Group as={Row} controlId="user-type" className="mb-4">
-                  <Col>
-                    <Form.Label>계정 유형</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="userType"
-                      required
-                      value={user.userType}
-                      onChange={handleChange}
-                    >
-                      <option value="">(계정 타입)</option>
-                      <option value="CUSTOMER">고객</option>
-                      <option value="WORKER">직원</option>
-                    </Form.Control>
-                  </Col>
-                </Form.Group>
-
-                {user.userType === "WORKER" && (
-                  <Form.Group>
-                    <Row>
-                      <Col>
-                        <WorkerDeptSelector
-                          workerDept={user.dept}
-                          onChange={handleChange}
-                        />
-                      </Col>
-                    </Row>
-                  </Form.Group>
-                )}
-
-                <div className="d-flex justify-content-center mb-3 mt-3 char2button">
-                  <Button
-                    type="submit"
-                    variant="outline-primary"
-                    size="sm"
-                    className="me-2"
-                    disabled={isProcessing}
-                  >
-                    {isProcessing ? (
-                      <ProcessSpinner message="유저 등록" />
-                    ) : (
-                      "등록"
-                    )}
-                  </Button>
-                  <Button
-                    variant="outline-info"
-                    size="sm"
-                    onClick={handleReset}
-                  >
-                    리셋
-                  </Button>
-                </div>
-
-                {isAdmin && (
-                  <div className="d-flex justify-content-center mb-3 mt-3">
-                    <Button
-                      variant="outline-warning"
-                      size="sm"
-                      onClick={easyData}
-                    >
-                      입력 편의
-                    </Button>
-                  </div>
-                )}
-                {alertSuccess && (
-                  <>
-                    <AlertMessage type="success" message={successMsg} />
-                    <p className="text-danger">
-                      *등록한 이메일에 로그인하여 메일 주소를 검증하십시오
-                    </p>
-                  </>
-                )}
-                {alertError && (
-                  <AlertMessage type="danger" message={errorMsg} />
-                )}
-                <div className="text-center">
-                  이미 등록한 경우:{" "}
-                  <Link to={"/login"} style={{ textDecoration: "none" }}>
-                    로그인
-                  </Link>
-                </div>
-              </Card.Body>
-            </Card>
-          </Form>
-        </Col>
-      </Row>
+            {isAdmin && (
+              <div className="d-flex justify-content-center mb-3 mt-3">
+                <Button variant="outline-warning" size="sm" onClick={easyData}>
+                  입력 편의
+                </Button>
+              </div>
+            )}
+            {alertSuccess && (
+              <>
+                <AlertMessage type="success" message={successMsg} />
+                <p className="text-danger">
+                  *등록한 이메일에 로그인하여 메일 주소를 검증하십시오
+                </p>
+              </>
+            )}
+            {alertError && <AlertMessage type="danger" message={errorMsg} />}
+            <div className="text-center">
+              이미 등록한 경우:{" "}
+              <Link to={"/login"} style={{ textDecoration: "none" }}>
+                로그인
+              </Link>
+            </div>
+          </Card.Body>
+        </Card>
+      </Form>
     </Container>
   );
 };
