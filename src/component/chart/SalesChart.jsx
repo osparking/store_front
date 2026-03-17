89a1eb20aaa8ccabfe73924d0a01f467fc2f9ad7
BumTiger <jbpark03@gmail.com>
2026-03-17 15:39:36 +0900

    비누 판매 자료 없음 표시 구현

--- src/component/chart/SalesChart.jsx
+++ src/component/chart/SalesChart.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from "react";
+import NoDataExists from "../common/NoDataExists";
 
 const SalesChart = () => {
-  return (
-    <div>비누 판매 집계</div>
-  )
-}
+  const [soapSold, setSoapSold] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-export default SalesChart
+  return (
+    <section className="mb-5">
+      <h5 className="chart-title mb-3">비누 판매 집계</h5>
+      {soapSold && soapSold.length > 0 ? (
+        <>
+          <ResponsiveContainer
+            width={"100%"}
+            height={300}
+          ></ResponsiveContainer>
+        </>
+      ) : (
+        <NoDataExists dataType={"판매"} errorMessage={errorMsg} />
+      )}
+    </section>
+  );
+};
+
+export default SalesChart;
