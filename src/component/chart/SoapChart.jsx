f47e73d62e523dcec8518991ae2bc4ce425f6036
BumTiger <jbpark03@gmail.com>
2026-03-17 15:34:26 +0900

    비누 생산 실적 자료 없음 표시 구현

--- src/component/chart/SoapChart.jsx
+++ src/component/chart/SoapChart.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from "react";
+import NoDataExists from "../common/NoDataExists";
 
 const SoapChart = () => {
-  return (
-    <div>비누 생산 실적</div>
-  )
-}
+  const [soapProduced, setSoapProduced] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-export default SoapChart
+  return (
+    <section className="mb-5">
+      <h5 className="chart-title mb-3">비누 생산 실적</h5>
+      {soapProduced && soapProduced.length > 0 ? (
+        <>
+          <ResponsiveContainer
+            width={"100%"}
+            height={300}
+          ></ResponsiveContainer>
+        </>
+      ) : (
+        <NoDataExists dataType={"생산"} errorMessage={errorMsg} />
+      )}
+    </section>
+  );
+};
+
+export default SoapChart;
