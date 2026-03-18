8b3d664328fef66ff05af90f2e26291b5b97c086
BumTiger <jbpark03@gmail.com>
2026-03-18 11:30:05 +0900

    관리자 개관쪽, 스크롤 재활용화 - 카드콤보

--- src/component/card/CardCompo.jsx
+++ src/component/card/CardCompo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const CardCompo = ({ label, count, IconCompo, onClick }) => {
+const CardCompo = ({ label, count, IconCompo, chartName, scrollToChart }) => {
   return (
-    <div className="admin-card header-card" onClick={onClick}>
+    <div
+      className="admin-card header-card"
+      onClick={() => scrollToChart(chartName)}
+    >
       <div className="card-inner">
         {label}
         <IconCompo className="card-icon" />
