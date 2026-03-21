a7b90568fcd49cf3c159f3e777978de0b44bc944
BumTiger <jbpark03@gmail.com>
2026-03-21 15:06:22 +0900

    최근 6개월 판매 자료 읽기 - 표출 전

--- src/component/chart/SalesChart.jsx
+++ src/component/chart/SalesChart.jsx
@@ -1,14 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NoDataExists from "../common/NoDataExists";
+import { callWithToken } from "../util/api";
+import { ResponsiveContainer } from "recharts";
 
 const SalesChart = () => {
-  const [soapSold, setSoapSold] = useState([]);
+  const [salesChartData, setSalesChartData] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    const getSalesChartData = async () => {
+      try {
+        const response = await callWithToken(
+          "get",
+          "/admin/soap_sale_chart",
+        );
+        const responseData = await response.data.data;
+        if (responseData) {
+          console.log("responseData:", responseData);
+          setSalesChartData(responseData);
+        } else {
+          navigate("/login");
+        }
+      } catch (err) {
+        setErrorMsg("판매 통계 자료 오류: ", err.message);
+      }
+    };
+    getSalesChartData();
+  }, []);
 
   return (
     <section className="mb-5">
       <h5 className="chart-title mb-3">비누 판매 집계</h5>
-      {soapSold && soapSold.length > 0 ? (
+      {salesChartData && salesChartData.length > 0 ? (
         <>
           <ResponsiveContainer
             width={"100%"}
