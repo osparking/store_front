ecf6f6ff6b035c6057cbe9c4f9f9d2aada6b2f09
BumTiger <jbpark03@gmail.com>
2026-04-02 15:44:18 +0900

    CustomerTable을 ManageCustomers와 CustomersTable로 분리

---
+++ src/component/admin/CustomersTable.jsx
@@ -0,0 +1,58 @@
+import {
+    OverlayTrigger,
+    Table,
+    Tooltip
+} from "react-bootstrap";
+import { BsEyeFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import CustomersTable from "./CustomersTable";
+import "./ManageCustomers.css";
+
+const CustomersTable = (displayCustomers) => {
+  return (
+    <Table bordered hover striped className="admin-customer-table">
+      <thead>
+        <tr>
+          <th>아이디</th>
+          <th>성명</th>
+          <th>휴대폰</th>
+          <th>이메일</th>
+          <th>인증</th>
+          <th className="minDateWidthLong">등록일시</th>
+          <th>유저유형</th>
+          <th colSpan={2}>작업</th>
+        </tr>
+      </thead>
+      <tbody>
+        {displayCustomers.map((customer, idx) => (
+          <tr key={idx}>
+            <td>{customer.id}</td>
+            <td>{customer.fullName}</td>
+            <td>{customer.mbPhone}</td>
+            <td>{customer.email}</td>
+            <td>{customer.enabled ? "예" : "아니오"}</td>
+            <td>{customer.addDate}</td>
+            <td>{customer.userType}</td>
+            <td>
+              <OverlayTrigger
+                overlay={
+                  <Tooltip id={`tooltip-view-${idx}`}>상세 보기</Tooltip>
+                }
+              >
+                <Link
+                  to={`/dashboard/${customer.id}/user`}
+                  className="text-info"
+                  state={{ userState: customer }}
+                >
+                  <BsEyeFill />
+                </Link>
+              </OverlayTrigger>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default CustomersTable;
--- src/component/admin/ManageCustomers.jsx
+++ src/component/admin/ManageCustomers.jsx
@@ -3,20 +3,16 @@ import {
   Col,
   Form,
   InputGroup,
-  OverlayTrigger,
-  Row,
-  Table,
-  Tooltip,
+  Row
 } from "react-bootstrap";
-import { BsEyeFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
 import AlertMessage from "../common/AlertMessage";
 import ItemFilter from "../common/ItemFilter";
 import Paginator from "../common/Paginator";
 import { getCustomerList } from "../customer/CustomerService";
 import BsAlertHook from "../hook/BsAlertHook";
-import "./CustomerTable.css";
 import { getRecordRange } from "../util/utilities";
+import CustomersTable from "./CustomersTable";
+import "./ManageCustomers.css";
 
 const CustomerTable = () => {
   const [customers, setCustomers] = useState([]);
@@ -152,48 +148,7 @@ const CustomerTable = () => {
         )}
       </p>
       <div className="table-responsive admin-customer-table-container">
-        <Table bordered hover striped className="admin-customer-table">
-          <thead>
-            <tr>
-              <th>아이디</th>
-              <th>성명</th>
-              <th>휴대폰</th>
-              <th>이메일</th>
-              <th>인증</th>
-              <th className="minDateWidthLong">등록일시</th>
-              <th>유저유형</th>
-              <th colSpan={2}>작업</th>
-            </tr>
-          </thead>
-          <tbody>
-            {displayCustomers.map((customer, idx) => (
-              <tr key={idx}>
-                <td>{customer.id}</td>
-                <td>{customer.fullName}</td>
-                <td>{customer.mbPhone}</td>
-                <td>{customer.email}</td>
-                <td>{customer.enabled ? "예" : "아니오"}</td>
-                <td>{customer.addDate}</td>
-                <td>{customer.userType}</td>
-                <td>
-                  <OverlayTrigger
-                    overlay={
-                      <Tooltip id={`tooltip-view-${idx}`}>상세 보기</Tooltip>
-                    }
-                  >
-                    <Link
-                      to={`/dashboard/${customer.id}/user`}
-                      className="text-info"
-                      state={{ userState: customer }}
-                    >
-                      <BsEyeFill />
-                    </Link>
-                  </OverlayTrigger>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        {CustomersTable(displayCustomers)}
       </div>
       <Paginator
         pageSize={pageSize}
@@ -208,3 +163,4 @@ const CustomerTable = () => {
 };
 
 export default CustomerTable;
+a;
