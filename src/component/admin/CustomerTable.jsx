7e4347df17e2d7a291e0d4292c7845406365c9a5
BumTiger <jbpark03@gmail.com>
2026-04-02 13:51:33 +0900

    고객 목록 테이블 성분 및 모양새 수정

--- src/component/admin/CustomerTable.jsx
+++ src/component/admin/CustomerTable.jsx
@@ -1,12 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Form, InputGroup, OverlayTrigger, Row, Table, Tooltip } from 'react-bootstrap';
-import { BsEyeFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import AlertMessage from '../common/AlertMessage';
-import ItemFilter from '../common/ItemFilter';
-import Paginator from '../common/Paginator';
-import { getCustomerList } from '../customer/CustomerService';
-import BsAlertHook from '../hook/BsAlertHook';
+import { useEffect, useState } from "react";
+import {
+  Col,
+  Form,
+  InputGroup,
+  OverlayTrigger,
+  Row,
+  Table,
+  Tooltip,
+} from "react-bootstrap";
+import { BsEyeFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import AlertMessage from "../common/AlertMessage";
+import ItemFilter from "../common/ItemFilter";
+import Paginator from "../common/Paginator";
+import { getCustomerList } from "../customer/CustomerService";
+import BsAlertHook from "../hook/BsAlertHook";
+import "./CustomerTable.css";
 
 const CustomerTable = () => {
   const [customers, setCustomers] = useState([]);
@@ -33,13 +42,14 @@ const CustomerTable = () => {
     }
   };
 
-  const emails = Array.from(new Set(customers.map(
-    (customer) => customer.email))).sort();
+  const emails = Array.from(
+    new Set(customers.map((customer) => customer.email)),
+  ).sort();
   const [selectedEmail, setSelectedEmail] = useState(
-    localStorage.getItem("SELECTED_EMAIL") || ""
+    localStorage.getItem("SELECTED_EMAIL") || "",
   );
   const [emailSubstr, setEmailSubstr] = useState(
-    localStorage.getItem("EMAIL_SUBSTR") || ""
+    localStorage.getItem("EMAIL_SUBSTR") || "",
   );
   const clearFilter = () => {
     setSelectedEmail("");
@@ -50,10 +60,13 @@ const CustomerTable = () => {
   const [filteredOnes, setFilteredOnes] = useState([]);
   useEffect(() => {
     if (selectedEmail) {
-      setFilteredOnes(customers.filter((customer) => customer.email === selectedEmail))
+      setFilteredOnes(
+        customers.filter((customer) => customer.email === selectedEmail),
+      );
     } else if (emailSubstr) {
-      setFilteredOnes(customers.filter(
-        (customer) => customer.email.includes(emailSubstr)));
+      setFilteredOnes(
+        customers.filter((customer) => customer.email.includes(emailSubstr)),
+      );
     } else {
       setFilteredOnes(customers);
     }
@@ -67,7 +80,7 @@ const CustomerTable = () => {
     }
     setSelectedEmail(value);
     localStorage.setItem("SELECTED_EMAIL", value);
-  }
+  };
 
   const handleEmailSubChg = (e) => {
     clearFilter();
@@ -81,7 +94,8 @@ const CustomerTable = () => {
   }, []);
 
   const [currCustomerPage, setCurrCustomerPage] = useState(
-    localStorage.getItem("CURR_CUST_PAGE") || 1);
+    localStorage.getItem("CURR_CUST_PAGE") || 1,
+  );
 
   const [pageSize] = useState(10);
   const idxLastPlus1 = currCustomerPage * pageSize;
@@ -91,18 +105,16 @@ const CustomerTable = () => {
   const setAndSavePageNo = (pageNo) => {
     setCurrCustomerPage(pageNo);
     localStorage.setItem("CURR_CUST_PAGE", pageNo);
-  }
+  };
 
   return (
-    <main>
+    <>
       <Row>
         <Col>
           {alertSuccess && (
             <AlertMessage type={"success"} message={successMsg} />
           )}
-          {alertError && (
-            <AlertMessage type={"danger"} message={errorMsg} />
-          )}
+          {alertError && <AlertMessage type={"danger"} message={errorMsg} />}
         </Col>
       </Row>
       <Row className="mb-2">
@@ -127,54 +139,60 @@ const CustomerTable = () => {
           </InputGroup>
         </Col>
       </Row>
-      <Table bordered hover striped>
-        <thead>
-          <tr>
-            <th>아이디</th>
-            <th>성명</th>
-            <th>휴대폰</th>
-            <th>이메일</th>
-            <th>인증</th>
-            <th>등록일시</th>
-            <th>유저유형</th>
-            <th colSpan={2}>작업</th>
-          </tr>
-        </thead>
-        <tbody>
-          {displayCustomers.map((customer, idx) => (
-            <tr key={idx}>
-              <td>{customer.id}</td>
-              <td>{customer.fullName}</td>
-              <td>{customer.mbPhone}</td>
-              <td>{customer.email}</td>
-              <td>{customer.enabled ? "예" : "아니오"}</td>
-              <td>{customer.addDate}</td>
-              <td>{customer.userType}</td>
-              <td>
-                <OverlayTrigger
-                  overlay={
-                    <Tooltip id={`tooltip-view-${idx}`}>상세 보기</Tooltip>
-                  }
-                >
-                  <Link
-                    to={`/dashboard/${customer.id}/user`}
-                    className="text-info"
-                    state={{ userState: customer }}>
-                    <BsEyeFill />
-                  </Link>
-                </OverlayTrigger>
-              </td>
+      <div className="table-responsive admin-customer-table-container">
+        <Table bordered hover striped className="admin-customer-table">
+          <thead>
+            <tr>
+              <th>아이디</th>
+              <th>성명</th>
+              <th>휴대폰</th>
+              <th>이메일</th>
+              <th>인증</th>
+              <th className="minDateWidthLong">등록일시</th>
+              <th>유저유형</th>
+              <th colSpan={2}>작업</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Paginator 
+          </thead>
+          <tbody>
+            {displayCustomers.map((customer, idx) => (
+              <tr key={idx}>
+                <td>{customer.id}</td>
+                <td>{customer.fullName}</td>
+                <td>{customer.mbPhone}</td>
+                <td>{customer.email}</td>
+                <td>{customer.enabled ? "예" : "아니오"}</td>
+                <td>{customer.addDate}</td>
+                <td>{customer.userType}</td>
+                <td>
+                  <OverlayTrigger
+                    overlay={
+                      <Tooltip id={`tooltip-view-${idx}`}>상세 보기</Tooltip>
+                    }
+                  >
+                    <Link
+                      to={`/dashboard/${customer.id}/user`}
+                      className="text-info"
+                      state={{ userState: customer }}
+                    >
+                      <BsEyeFill />
+                    </Link>
+                  </OverlayTrigger>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <Paginator
         pageSize={pageSize}
         totalItems={filteredOnes.length}
+        totalPages={Math.ceil(filteredOnes.length / pageSize)}
         currPage={currCustomerPage}
-        setCurrPage={setAndSavePageNo}/>
-    </main>
+        setCurrPage={setAndSavePageNo}
+        darkBackground={true}
+      />
+    </>
   );
-}
+};
 
-export default CustomerTable
+export default CustomerTable;
