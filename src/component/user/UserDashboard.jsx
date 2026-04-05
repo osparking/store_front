44de5c1ecc964d55bac505bc9b361ea6baf4f2b0
BumTiger <jbpark03@gmail.com>
2026-04-05 20:02:02 +0900

    3 종 장치 너비별 유저 대시보드 차별 구현

--- src/component/user/UserDashboard.jsx
+++ src/component/user/UserDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Tab, Tabs } from "react-bootstrap";
+import { Container, Dropdown, Tab, Tabs } from "react-bootstrap";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import ManageQuestions from "../admin/ManageQuestions";
 import AlertMessage from "../common/AlertMessage";
@@ -7,11 +7,12 @@ import BsAlertHook from "../hook/BsAlertHook";
 import { deleteUserPhoto } from "../modal/ImageService";
 import ManageMyOrder from "./ManageMyOrder";
 import OverviewUser from "./OverviewUser";
-import "./UserDashboard.css";
+import "./userDashboard.css";
 import UserProfile from "./UserProfile";
 import { getUserDtoById } from "./UserService";
 import ManageMyReviews from "./review/ManageMyReviews";
 import "../../index.css";
+import { useMediaQuery } from "react-responsive";
 
 const UserDashboard = () => {
   const location = useLocation();
@@ -75,35 +76,113 @@ const UserDashboard = () => {
     }
   };
 
-  return (
-    <Container fluid className="home-container user-dashboard">
-      <Tabs
-        activeKey={activeKey}
-        onSelect={changeActiveKey}
-        className="tabBackgroundThick contentHolyCentered"
-      >
-        <Tab eventKey="profile" title={<h5>내 프로필</h5>}>
-          {alertError && <AlertMessage type={"danger"} message={errorMsg} />}
-          {alertSuccess && (
-            <AlertMessage type={"success"} message={successMsg} />
-          )}
-          {user && <UserProfile user={user} handleRemovePhoto={removePhoto} />}
-        </Tab>
-        <Tab eventKey="purchase_stat" title={<h5>구매 통계</h5>}>
-          <OverviewUser />
-        </Tab>
-        <Tab eventKey="purchase_list" title={<h5>나의 주문</h5>}>
-          <ManageMyOrder />
-        </Tab>
-        <Tab eventKey="my_question" title={<h5>나의 질문</h5>}>
-          <ManageQuestions mine />
-        </Tab>
-        <Tab eventKey="my_review" title={<h5>나의 리뷰</h5>}>
-          <ManageMyReviews />
-        </Tab>
-      </Tabs>
-    </Container>
-  );
+  const isVeryNarrow = useMediaQuery({ maxWidth: 480 });
+  const isMedium = useMediaQuery({ minWidth: 481, maxWidth: 768 });
+  const tabItems = [
+    {
+      key: "profile",
+      title: "내 프로필",
+      component: user && (
+        <UserProfile user={user} handleRemovePhoto={removePhoto} />
+      ),
+    },
+    { key: "purchase_stat", title: "구매 통계", component: <OverviewUser /> },
+    { key: "purchase_list", title: "나의 주문", component: <ManageMyOrder /> },
+    {
+      key: "my_question",
+      title: "나의 질문",
+      component: <ManageQuestions mine />,
+    },
+    { key: "my_review", title: "나의 리뷰", component: <ManageMyReviews /> },
+  ];
+
+  const currentComponent = tabItems.find(
+    (item) => item.key === activeKey,
+  )?.component;
+
+  if (isVeryNarrow) {
+    return (
+      <Container fluid className="home-container user-dashboard">
+        <div className="mobile-tab-header">
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="outline-primary"
+              className="hamburger-menu"
+            >
+              ☰ {tabItems.find((item) => item.key === activeKey)?.title}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {tabItems.map((item) => (
+                <Dropdown.Item
+                  key={item.key}
+                  active={activeKey === item.key}
+                  onClick={() => changeActiveKey(item.key)}
+                >
+                  {item.title}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        <div className="mobile-tab-content">
+          {alertError && <AlertMessage type="danger" message={errorMsg} />}
+          {alertSuccess && <AlertMessage type="success" message={successMsg} />}
+          {currentComponent}
+        </div>
+      </Container>
+    );
+  } else if (isMedium) {
+    return (
+      <Container fluid className="home-container user-dashboard">
+        <Tabs
+          activeKey={activeKey}
+          onSelect={changeActiveKey}
+          className="tabBackgroundThick contentHolyCentered scrollable-tabs"
+          id="scrollable-tabs"
+        >
+          {tabItems.map((item) => (
+            <Tab
+              className="mediumWindowDashboardTab"
+              key={item.key}
+              eventKey={item.key}
+              title={<h5>{item.title}</h5>}
+            >
+              {alertError && <AlertMessage type="danger" message={errorMsg} />}
+              {alertSuccess && (
+                <AlertMessage type="success" message={successMsg} />
+              )}
+              {item.component}
+            </Tab>
+          ))}
+        </Tabs>
+      </Container>
+    );
+  } else {
+    return (
+      <Container fluid className="home-container user-dashboard">
+        <Tabs
+          activeKey={activeKey}
+          onSelect={changeActiveKey}
+          className="tabBackgroundThick contentHolyCentered"
+        >
+          {tabItems.map((item) => (
+            <Tab
+              key={item.key}
+              eventKey={item.key}
+              title={<h5>{item.title}</h5>}
+            >
+              {alertError && <AlertMessage type="danger" message={errorMsg} />}
+              {alertSuccess && (
+                <AlertMessage type="success" message={successMsg} />
+              )}
+              {item.component}
+            </Tab>
+          ))}
+        </Tabs>
+      </Container>
+    );
+  }
 };
 
 export default UserDashboard;
