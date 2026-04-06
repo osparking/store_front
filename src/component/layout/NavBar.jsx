13814008c846a526875ab127dd25a1439dc4600a
BumTiger <jbpark03@gmail.com>
2026-04-06 11:21:57 +0900

    최상 햄버거 좌측 활성 메뉴 텍스트 표시 구현

--- src/component/layout/NavBar.jsx
+++ src/component/layout/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { logoutUser } from "../auth/AuthService";
 import { getStorageToken } from "../util/utilities";
 import "./navBar.css";
@@ -43,6 +43,28 @@ const NavBar = () => {
     checkIfAdmin();
   }, []);
 
+  const [activeLinkText, setActiveLinkText] = useState("");
+  const location = useLocation();
+
+  const getActiveLinkText = (pathname) => {
+    if (pathname === "/") return "홈 페이지";
+    if (pathname === "/soap_intro") return "비누 소개";
+    if (pathname === "/buy_soap") return "비누 주문";
+    if (pathname === "/question") return "질문하기";
+    if (pathname === "/register_user") return "등록";
+    if (pathname.startsWith("/dashboard/admin")) return "관리 업무";
+    if (pathname === "/work_item") return "직원 직무";
+    if (pathname === "/login") return "로그인";
+    if (pathname.startsWith("/dashboard/") && pathname.includes("/user"))
+      return "대시보드";
+    if (pathname.includes("/update")) return "내 정보 수정";
+    return "";
+  };
+
+  useEffect(() => {
+    setActiveLinkText(getActiveLinkText(location.pathname));
+  }, [location]);
+
   return (
     <Navbar expand="lg" sticky="top" className="nav-bg start-0 end-0">
       <Container>
@@ -53,7 +75,15 @@ const NavBar = () => {
         >
           범이비누
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Toggle
+          id="mainHamburger"
+          aria-controls="responsive-navbar-nav"
+        >
+          <div className="active-link-text-mobile">{activeLinkText}</div>
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link
