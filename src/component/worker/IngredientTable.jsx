35ba10c2049a3304a868e17c77d28ba4286abc1a
BumTiger <jbpark03@gmail.com>
2026-05-08 11:59:07 +0900

    재료 관리 성분에서 재료 테이블 성분 분리

---
+++ src/component/worker/IngredientTable.jsx
@@ -0,0 +1,123 @@
+import { Button, OverlayTrigger, Table, Tooltip } from "react-bootstrap";
+import { BsPencilFill, BsTrashFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const IngredientTable = (ingreList) => {
+  function isValidUrl(string) {
+    try {
+      new URL(string);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function getDomain(url) {
+    let domain = new URL(url);
+    domain = domain.hostname;
+    return domain;
+  }
+
+  return (
+    <div id="ingredient-table-container">
+      <Table
+        id="workerJobTable"
+        bordered
+        hover
+        striped
+        style={{
+          tableLayout: "fixed",
+          minWidth: "730px",
+        }}
+      >
+        <colgroup>
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "13%" }} />
+          <col style={{ width: "9%" }} />
+          <col style={{ width: "14%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "11%" }} />
+          <col style={{ width: "14%" }} />
+          <col style={{ width: "9%" }} />
+          <col style={{ width: "5%" }} />
+          <col style={{ width: "5%" }} />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>순번</th>
+            <th>재료명</th>
+            <th>입고일</th>
+            <th>구매처</th>
+            <th>용량</th>
+            <th>단위</th>
+            <th>수량</th>
+            <th>사용기한</th>
+            <th>입력일시</th>
+            <th>직원명</th>
+            <th colSpan={2}>작업</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ingreList.map((ingredient, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{ingredient.ingreName}</td>
+              <td>{ingredient.storeDate}</td>
+              <td>
+                {isValidUrl(ingredient.buyPlace) ? (
+                  <Link to={ingredient.buyPlace} target="_blank">
+                    {getDomain(ingredient.buyPlace)}
+                  </Link>
+                ) : (
+                  ingredient.buyPlace
+                )}
+              </td>
+              <td>{ingredient.quantity}</td>
+              <td>{ingredient.packunit}</td>
+              <td>{ingredient.count}</td>
+              <td>{ingredient.expireDate}</td>
+              <td>{ingredient.addTime}</td>
+              <td>{ingredient.workerName}</td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>정보 편집</Tooltip>
+                  }
+                >
+                  <Button
+                    size="sm"
+                    style={{ backgroundColor: "transparent", border: "none" }}
+                    onClick={() => openWithRow(ingredient)}
+                  >
+                    <BsPencilFill className="text-success" />
+                  </Button>
+                </OverlayTrigger>
+              </td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>
+                      입고 기록 삭제
+                    </Tooltip>
+                  }
+                >
+                  <Link
+                    to={"#"}
+                    className="text-danger"
+                    onClick={() => handleShowDelModal(ingredient.id)}
+                  >
+                    <BsTrashFill />
+                  </Link>
+                </OverlayTrigger>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default IngredientTable;
