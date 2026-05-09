86ef76c023f7c2fa46eeb7738f94d8760a53b9d3
BumTiger <jbpark03@gmail.com>
2026-05-09 21:12:55 +0900

    직원 비누 생산 관리에서 분리된 생산기록 테이블

---
+++ src/component/worker/produce/ProduceTable.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Button, OverlayTrigger, Table, Tooltip } from "react-bootstrap";
+import { BsPencilFill, BsTrashFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const ProduceTable = (produceRows) => {
+  return (
+    <div id="produce-table-container">
+      <Table
+        id="workerJobTable"
+        bordered
+        hover
+        striped
+        style={{
+          minWidth: "730px",
+        }}
+      >
+        <thead>
+          <tr>
+            <th>순번</th>
+            <th>외형</th>
+            <th>수량(개)</th>
+            <th>생산일</th>
+            <th>생산자 (ID)</th>
+            <th>등록자 (ID)</th>
+            <th>등록일시</th>
+            <th colSpan={2}>작업</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produceRows.map((produce, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{produce.shape}</td>
+              <td>{produce.quantity}</td>
+              <td>{produce.produceDate}</td>
+              <td>
+                {produce.producerName} ({produce.producerId})
+              </td>
+              <td>
+                {produce.registerName} ({produce.registerId})
+              </td>
+              <td>{produce.registerTime}</td>
+              <td style={{ width: "50px" }}>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>정보 편집</Tooltip>
+                  }
+                >
+                  <Button
+                    size="sm"
+                    style={{ backgroundColor: "transparent", border: "none" }}
+                    onClick={() => editProduceInfo(produce)}
+                  >
+                    <BsPencilFill className="text-success" />
+                  </Button>
+                </OverlayTrigger>
+              </td>
+              <td style={{ width: "50px" }}>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>
+                      입고 기록 삭제
+                    </Tooltip>
+                  }
+                >
+                  <Link
+                    to={"#"}
+                    className="text-danger"
+                    onClick={() => handleShowDelModal(produce)}
+                  >
+                    <BsTrashFill />
+                  </Link>
+                </OverlayTrigger>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default ProduceTable;
