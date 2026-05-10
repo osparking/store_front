168f44c4d32bfa750128dd4790b6aa5590b3ebe8
BumTiger <jbpark03@gmail.com>
2026-05-10 09:21:23 +0900

    직원직무. 비누 생산 기록 테이블 헤더 바디 분리

--- src/component/worker/produce/ProduceTable.jsx
+++ src/component/worker/produce/ProduceTable.jsx
@@ -1,82 +1,93 @@
-import React from "react";
-import { Button, OverlayTrigger, Table, Tooltip } from "react-bootstrap";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { BsPencilFill, BsTrashFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import "../WorkerCanvas.css";
+import "../WorkerTable.css";
 
 const ProduceTable = (produceRows) => {
   return (
-    <div id="produce-table-container">
-      <Table
-        id="workerJobTable"
-        bordered
-        hover
-        striped
-        style={{
-          minWidth: "730px",
-        }}
-      >
-        <thead>
-          <tr>
-            <th>순번</th>
-            <th>외형</th>
-            <th>수량(개)</th>
-            <th>생산일</th>
-            <th>생산자 (ID)</th>
-            <th>등록자 (ID)</th>
-            <th>등록일시</th>
-            <th colSpan={2}>작업</th>
-          </tr>
-        </thead>
-        <tbody>
-          {produceRows.map((produce, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{produce.shape}</td>
-              <td>{produce.quantity}</td>
-              <td>{produce.produceDate}</td>
-              <td>
-                {produce.producerName} ({produce.producerId})
-              </td>
-              <td>
-                {produce.registerName} ({produce.registerId})
-              </td>
-              <td>{produce.registerTime}</td>
-              <td style={{ width: "50px" }}>
-                <OverlayTrigger
-                  overlay={
-                    <Tooltip id={`tooltip-view-${index}`}>정보 편집</Tooltip>
-                  }
-                >
-                  <Button
-                    size="sm"
-                    style={{ backgroundColor: "transparent", border: "none" }}
-                    onClick={() => editProduceInfo(produce)}
-                  >
-                    <BsPencilFill className="text-success" />
-                  </Button>
-                </OverlayTrigger>
-              </td>
-              <td style={{ width: "50px" }}>
-                <OverlayTrigger
-                  overlay={
-                    <Tooltip id={`tooltip-view-${index}`}>
-                      입고 기록 삭제
-                    </Tooltip>
-                  }
-                >
-                  <Link
-                    to={"#"}
-                    className="text-danger"
-                    onClick={() => handleShowDelModal(produce)}
-                  >
-                    <BsTrashFill />
-                  </Link>
-                </OverlayTrigger>
-              </td>
+    <div className="worker-table-wrapper">
+      <div className="table-header">
+        <table
+          className="table table-bordered table-hover table-striped"
+          style={{
+            tableLayout: "fixed",
+            minWidth: "730px",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>순번</th>
+              <th>외형</th>
+              <th>수량(개)</th>
+              <th>생산일</th>
+              <th>생산자 (ID)</th>
+              <th>등록자 (ID)</th>
+              <th>등록일시</th>
+              <th colSpan={2}>작업</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+        </table>
+      </div>
+      <div className="table-body produce">
+        <table
+          className="table table-bordered table-hover table-striped"
+          style={{
+            tableLayout: "fixed",
+            minWidth: "730px",
+          }}
+        >
+          <tbody>
+            {produceRows.map((produce, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{produce.shape}</td>
+                <td>{produce.quantity}</td>
+                <td>{produce.produceDate}</td>
+                <td>
+                  {produce.producerName} ({produce.producerId})
+                </td>
+                <td>
+                  {produce.registerName} ({produce.registerId})
+                </td>
+                <td>{produce.registerTime}</td>
+                <td style={{ width: "50px" }}>
+                  <OverlayTrigger
+                    overlay={
+                      <Tooltip id={`tooltip-view-${index}`}>정보 편집</Tooltip>
+                    }
+                  >
+                    <Button
+                      size="sm"
+                      style={{ backgroundColor: "transparent", border: "none" }}
+                      onClick={() => editProduceInfo(produce)}
+                    >
+                      <BsPencilFill className="text-success" />
+                    </Button>
+                  </OverlayTrigger>
+                </td>
+                <td style={{ width: "50px" }}>
+                  <OverlayTrigger
+                    overlay={
+                      <Tooltip id={`tooltip-view-${index}`}>
+                        입고 기록 삭제
+                      </Tooltip>
+                    }
+                  >
+                    <Link
+                      to={"#"}
+                      className="text-danger"
+                      onClick={() => handleShowDelModal(produce)}
+                    >
+                      <BsTrashFill />
+                    </Link>
+                  </OverlayTrigger>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
