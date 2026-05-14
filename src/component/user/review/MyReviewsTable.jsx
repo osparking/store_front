ca78cb26e0f7f91baa8373b228a2260f98384ed8
BumTiger <jbpark03@gmail.com>
2026-05-14 20:43:32 +0900

    나의 리뷰 테이블 헤더 바디 스크롤 분리

--- src/component/user/review/MyReviewsTable.jsx
+++ src/component/user/review/MyReviewsTable.jsx
@@ -1,37 +1,64 @@
-import { Table } from "react-bootstrap";
+import "../../../index.css";
 import { formatDate } from "../../util/utilities";
+import "../userDashboard.css";
+import "./MyReviewsTable.css";
 
 const MyReviewsTable = (reviews, manageReview) => {
+  const reviewTableWidth = "730px";
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr className="userTableHeader">
-          <th>주문명</th>
-          <th className="minDateWidth">주문일시</th>
-          <th>별점</th>
-          <th>후기 시작 부분</th>
-          <th className="minDateWidth">후기 작성일시</th>
-          <th>주문ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        {reviews &&
-          reviews.map((review, idx) => (
-            <tr key={idx}>
-              <td>{review.orderName}</td>
-              <td>{formatDate(review.orderTime)}</td>
-              <td>{review.stars}</td>
-              <td className="text-start">
-                <a href="#" onClick={() => manageReview(review)}>
-                  {review.reviewPreview}
-                </a>
-              </td>
-              <td>{formatDate(review.reviewTime)}</td>
-              <td className="text-center">{review.id}</td>
+    <div className="user-table-wrapper">
+      <div className="table-header">
+        <table
+          className="table table-bordered table-hover table-striped"
+          style={{
+            tableLayout: "fixed",
+            width: "100%",
+            minWidth: reviewTableWidth,
+            width: reviewTableWidth,
+          }}
+        >
+          <thead>
+            <tr>
+              <th>주문명</th>
+              <th>주문일시</th>
+              <th>별점</th>
+              <th>후기 시작 부분</th>
+              <th>후기 작성일시</th>
+              <th>주문ID</th>
             </tr>
-          ))}
-      </tbody>
-    </Table>
+          </thead>
+        </table>
+      </div>
+      <div className="table-body my-review">
+        <table
+          className="table table-bordered table-hover table-striped"
+          style={{
+            tableLayout: "fixed",
+            minWidth: reviewTableWidth,
+            width: reviewTableWidth,
+          }}
+        >
+          <tbody>
+            {reviews &&
+              reviews.map((review, idx) => (
+                <tr key={idx}>
+                  <td>{review.orderName}</td>
+                  <td>{formatDate(review.orderTime)}</td>
+                  <td>{review.stars}</td>
+                  <td className="text-start">
+                    <a href="#" onClick={() => manageReview(review)}>
+                      {review.reviewPreview}
+                    </a>
+                  </td>
+                  <td>{formatDate(review.reviewTime)}</td>
+                  <td className="text-center">{review.id}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
