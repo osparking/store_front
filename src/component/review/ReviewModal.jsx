ede3349009813e152c7e4bc53c4869a28fbd4935
BumTiger <jbpark03@gmail.com>
2026-05-30 14:49:38 +0900

    나의주문에서 리뷰의 삭제가 나의리뷰 테이블에 반영됨

--- src/component/review/ReviewModal.jsx
+++ src/component/review/ReviewModal.jsx
@@ -42,6 +42,7 @@ export default function ReviewModal({
         `/order/${orderId}/delete_review`,
       );
       toast.success("후기 삭제 완료");
+      refreshReviews();
       handleClose(true);
     } finally {
       setLoading(false);
