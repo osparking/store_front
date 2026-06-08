18a301272e3ce1c53d9d9aadb771cad80507518c
BumTiger <jbpark03@gmail.com>
2026-06-08 19:20:21 +0900

    후단에서 넘어온 반응 상태 상수 몇가지 정의

--- src/component/util/utilities.js
+++ src/component/util/utilities.js
@@ -216,3 +216,13 @@ export const maskEmail = (email) => {
     return firstThree + '*'.repeat(rest.length) + '@';
   });
 }
+
+export const HTTP_STATUS = {
+  OK: 200,
+  ACCEPTED: 202,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
