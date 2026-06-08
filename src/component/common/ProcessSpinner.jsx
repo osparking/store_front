3b5f58ec47c09a76a623fca7f8fc5c8bed3cca95
BumTiger <jbpark03@gmail.com>
2026-06-08 11:27:24 +0900

    진행중 표시 스피너 메시지 위, 중앙 배치

--- src/component/common/ProcessSpinner.jsx
+++ src/component/common/ProcessSpinner.jsx
@@ -3,16 +3,31 @@ import { Spinner } from "react-bootstrap";
 
 const ProcessSpinner = ({ size = "sm", animation = "grow", message = "" }) => {
   return (
-    <div className="text-center">
-      <Spinner
-        as="span"
-        animation={animation}
-        size={size}
-        role="status"
-        aria-hidden="true"
-      />
-      {message && <span className="sr-only">{message}&nbsp;중...</span>}
-    </div>
+    <>
+      <div className="text-center" style={{ zIndex: "10" }}>
+        <Spinner
+          as="span"
+          animation={animation}
+          size={size}
+          role="status"
+          aria-hidden="true"
+        />
+      </div>
+
+      {message && (
+        <div
+          className="sr-only"
+          aria-live="polite"
+          style={{
+            position: "absolute",
+            color: "white",
+            zIndex: "9",
+          }}
+        >
+          {message}&nbsp;중...
+        </div>
+      )}
+    </>
   );
 };
 
