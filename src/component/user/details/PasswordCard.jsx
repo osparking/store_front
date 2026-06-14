ee1d58aff9704ab7953122e0fdb6f4e4bdb07275
BumTiger <jbpark03@gmail.com>
2026-06-14 16:04:52 +0900

    유저 프로필 패스워드 카드 별도 성분화

---
+++ src/component/user/details/PasswordCard.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import EmpImage from "../../common/EmpImage";
+import ChangePassword from "../../modal/ChangePassword";
+import DeleteConfirmModal from "../../modal/DeleteConfirmModal";
+import DisableAccountModal from "../../modal/DisableAccountModal";
+import { deleteUserPhoto } from "../../modal/ImageService";
+import ImageUp from "../../modal/ImageUp";
+import "./../UserProfile.css";
+
+const PasswordCard = ({ user }) => {
+  const [showPhotoDelModal, setShowPhotoDelModal] = useState(false);
+  const confirmPhotoRemoval = () => {
+    setShowPhotoDelModal(true);
+  };
+
+  const [showImageUp, setShowImageUp] = useState(false);
+  const [showChangePassword, setShowChangePassword] = useState(false);
+
+  const [showDelModal, setShowDelModal] = useState(false);
+  const handleCloseAccountButtonCLick = () => {
+    setShowDelModal(true);
+  };
+  const handleModalXButtonClick = () => {
+    setShowDelModal(false);
+  };
+
+  const [delPhotoBtnDisabled, setDelPhotoBtnDisabled] = useState(false);
+  const removePhoto = async () => {
+    try {
+      setDelPhotoBtnDisabled(true);
+      const result = await deleteUserPhoto(user.id);
+      window.location.reload();
+    } catch (error) {
+      setErrorMsg(error.response.data.message);
+      setAlertError(true);
+    } finally {
+      setDelPhotoBtnDisabled(false);
+    }
+  };
+
+  const loginId = localStorage.getItem("LOGIN_ID");
+
+  return (
+    <Card
+      id="passwordCard"
+      className="text-center mb-3 shadow"
+      style={{ height: "fit-content" }}
+    >
+      <DisableAccountModal
+        show={showDelModal}
+        onHide={handleModalXButtonClick}
+        userId={loginId}
+        target={""}
+        disabled={false}
+        modalClass={"disable-account-confirm"}
+      />
+      <Card.Body className="p-3">
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          {!(user.userType === "고객") && (
+            <>
+              <EmpImage empPhoto={user.photoBytes} />
+              <p className="mt-5">
+                <Link to={"#"} onClick={() => setShowImageUp(true)}>
+                  {user.photoBytes ? "사진 변경" : "사진 등록"}
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to={"#"}
+                  {...(user.photoId
+                    ? { onClick: confirmPhotoRemoval }
+                    : { style: { cursor: "default", color: "grey" } })}
+                >
+                  사진 제거
+                </Link>
+              </p>
+              <ImageUp
+                user={user}
+                show={showImageUp}
+                handleClose={() => setShowImageUp(false)}
+              />
+            </>
+          )}
+          <p id="changePasswordLink">
+            <Link to={"#"} onClick={() => setShowChangePassword(true)}>
+              비밀번호 변경
+            </Link>
+          </p>
+          <ChangePassword
+            userId={user.id}
+            show={showChangePassword}
+            handleClose={() => setShowChangePassword(false)}
+          />
+        </div>
+        <div className="d-flex justify-content-center mt-2 mb-2">
+          <div className="mx-2">
+            <Button
+              variant="danger"
+              size="sm"
+              onClick={handleCloseAccountButtonCLick}
+              className="w-100"
+              style={{ minWidth: "60px" }}
+            >
+              비활성화
+            </Button>
+          </div>
+        </div>
+      </Card.Body>
+      <DeleteConfirmModal
+        show={showPhotoDelModal}
+        onHide={() => setShowPhotoDelModal(false)}
+        handleDeletion={removePhoto}
+        target={"프로필 사진"}
+        disabled={delPhotoBtnDisabled}
+        modalClass="delete-photo-confirm"
+      />
+    </Card>
+  );
+};
+
+export default PasswordCard;
