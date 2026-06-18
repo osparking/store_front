05cda68753d4885a544cef4787c59e0702aca55d
BumTiger <jbpark03@gmail.com>
2026-06-18 13:03:30 +0900

    직원 활성화 변경 기능 구현 - 직원 테이블 재 적재

--- src/component/admin/WorkersTable.jsx
+++ src/component/admin/WorkersTable.jsx
@@ -7,100 +7,138 @@ import {
   BsUnlockFill,
 } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import BsAlertHook from "../hook/BsAlertHook";
+import { callWithToken } from "../util/api";
 import { insert2Hyphens } from "../util/utilities";
 import "./AdminCanvas.css";
 import "./WorkersTable.css";
+import AlertMessage from "../common/AlertMessage";
 
-const WorkersTable = (displayWorkers, showAccountDetails) => {
+const WorkersTable = (displayWorkers, showAccountDetails, readWorkerList) => {
+  const {
+    successMsg,
+    setSuccessMsg,
+    alertSuccess,
+    setAlertSuccess,
+    errorMsg,
+    setErrorMsg,
+    alertError,
+    setAlertError,
+  } = BsAlertHook();
+
+  const handleLockToggle = async (worker) => {
+    try {
+      const result = await callWithToken(
+        "put",
+        `/admin/worker/${worker.id}/toggle`,
+      );
+      readWorkerList();
+      setAlertError(false);
+      setSuccessMsg(result.data.message + ", 활성값: " + !worker.enabled);
+      setAlertSuccess(true);
+    } catch (e) {
+      console.error("e:", e);
+      setErrorMsg(e.response.data.message);
+      setAlertSuccess(false);
+      setAlertError(true);
+    }
+  };
+
   return (
-    <Table bordered hover striped className="admin-worker-table">
-      <thead>
-        <tr>
-          <th>성명</th>
-          <th>이메일</th>
-          <th>휴대폰</th>
-          <th>소속</th>
-          <th>계정상태</th>
-          <th className="minDateWidthLong">등록일시</th>
-          <th>사진</th>
-          <th colSpan={4}>작업</th>
-        </tr>
-      </thead>
-      <tbody>
-        {displayWorkers.map((worker, index) => (
-          <tr key={index}>
-            <td>{worker.fullName}</td>
-            <td>{worker.email}</td>
-            <td>{insert2Hyphens(worker.mbPhone)}</td>
-            <td>{worker.dept}</td>
-            <td>{worker.enabled ? "활성" : "비활성"}</td>
-            <td>{worker.addDate}</td>
-            <td>{worker.photoId ? "유" : "무"}</td>
-            <td>
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id={`tooltip-view-${index}`}>상세 보기</Tooltip>
-                }
-              >
-                <Button
-                  className="readEye"
-                  onClick={() =>
-                    showAccountDetails({ worker: worker, editable: false })
-                  }
-                >
-                  <BsEyeFill />
-                </Button>
-              </OverlayTrigger>
-            </td>
-            <td>
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id={`tooltip-view-${index}`}>정보 수정</Tooltip>
-                }
-              >
-                <Button
-                  className="readEye"
-                  onClick={() =>
-                    showAccountDetails({ worker: worker, editable: true })
-                  }
-                >
-                  <BsPencilFill />
-                </Button>
-              </OverlayTrigger>
-            </td>
-            <td>
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id={`tooltip-view-${index}`}>
-                    {worker.enabled ? "계정 잠금" : "잠금 해제"}
-                  </Tooltip>
-                }
-              >
-                <span
-                  onClick={() => handleLockToggle(worker)}
-                  style={{ cursor: "pointer" }}
-                >
-                  {worker.enabled ? <BsUnlockFill /> : <BsLockFill />}
-                </span>
-              </OverlayTrigger>
-            </td>
-            <td>
-              <OverlayTrigger
-                overlay={<Tooltip id={`tooltip-view-${index}`}>삭제</Tooltip>}
-              >
-                <Link
-                  to={"#"}
-                  className="text-danger"
-                  onClick={() => processDeletion(worker.id, worker.fullName)}
-                >
-                  <BsTrashFill />
-                </Link>
-              </OverlayTrigger>
-            </td>
+    <>
+      <Table bordered hover striped className="admin-worker-table">
+        <thead>
+          <tr>
+            <th>성명</th>
+            <th>이메일</th>
+            <th>휴대폰</th>
+            <th>소속</th>
+            <th>계정상태</th>
+            <th className="minDateWidthLong">등록일시</th>
+            <th>사진</th>
+            <th colSpan={4}>작업</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {displayWorkers.map((worker, index) => (
+            <tr key={index}>
+              <td>{worker.fullName}</td>
+              <td>{worker.email}</td>
+              <td>{insert2Hyphens(worker.mbPhone)}</td>
+              <td>{worker.dept}</td>
+              <td>{worker.enabled ? "활성" : "비활성"}</td>
+              <td>{worker.addDate}</td>
+              <td>{worker.photoId ? "유" : "무"}</td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>상세 보기</Tooltip>
+                  }
+                >
+                  <Button
+                    className="readEye"
+                    onClick={() =>
+                      showAccountDetails({ worker: worker, editable: false })
+                    }
+                  >
+                    <BsEyeFill />
+                  </Button>
+                </OverlayTrigger>
+              </td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>정보 수정</Tooltip>
+                  }
+                >
+                  <Button
+                    className="readEye"
+                    onClick={() =>
+                      showAccountDetails({ worker: worker, editable: true })
+                    }
+                  >
+                    <BsPencilFill />
+                  </Button>
+                </OverlayTrigger>
+              </td>
+              <td>
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id={`tooltip-view-${index}`}>
+                      {worker.enabled ? "계정 잠금" : "잠금 해제"}
+                    </Tooltip>
+                  }
+                >
+                  <span
+                    onClick={() => handleLockToggle(worker)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {worker.enabled ? <BsUnlockFill /> : <BsLockFill />}
+                  </span>
+                </OverlayTrigger>
+              </td>
+              <td>
+                <OverlayTrigger
+                  overlay={<Tooltip id={`tooltip-view-${index}`}>삭제</Tooltip>}
+                >
+                  <Link
+                    to={"#"}
+                    className="text-danger"
+                    onClick={() => processDeletion(worker.id, worker.fullName)}
+                  >
+                    <BsTrashFill />
+                  </Link>
+                </OverlayTrigger>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {alertSuccess && <AlertMessage type={"success"} message={successMsg} />}
+        {alertError && <AlertMessage type={"danger"} message={errorMsg} />}
+      </div>
+    </>
   );
 };
 
