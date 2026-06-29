3c14fa0e68aa5794040526a7fcaf97469a4617d7
BumTiger <jbpark03@gmail.com>
2026-06-29 16:01:20 +0900

    두 (단가, 배송) 카드 좌우로 나란히 표시

--- src/component/admin/ManagePriceFee.jsx
+++ src/component/admin/ManagePriceFee.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
 import DeliveryFeeCard from "./price_fee/DeliveryFeeCard";
 import SoapPriceCard from "./price_fee/SoapPriceCard";
 
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+}
+
 const ManagePriceFee = () => {
+  const width = useWindowWidth();
+
   return (
     <div style={{ width: "100%", maxWidth: "700px", margin: "0 auto" }}>
-      <div>
-        <SoapPriceCard />
-      </div>
-      <div>
-        <DeliveryFeeCard />
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          flexDirection: width < 700 ? "column" : "row",
+        }}
+      >
+        <div style={{ flex: 1 }}>
+          <SoapPriceCard />
+        </div>
+        <div style={{ flex: 1 }}>
+          <DeliveryFeeCard />
+        </div>
       </div>
     </div>
   );
