dc5ac3e3aac9912beb6f89b4a475c28130dabcde
BumTiger <jbpark03@gmail.com>
2026-06-29 16:17:48 +0900

    배송비 관련 자료 갱신 기능 카드 골격

--- src/component/admin/price_fee/DeliveryFeeCard.jsx
+++ src/component/admin/price_fee/DeliveryFeeCard.jsx
@@ -1,10 +1,63 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button, Card, Form, Table } from "react-bootstrap";
+import { getFeeEtc } from "../../buy/orderService";
 
 const DeliveryFeeCard = () => {
+  const [feeEtc, setFeeEtc] = useState([]);
+  const [originFeeEtc, setOriginPrices] = useState([]);
+
   useEffect(() => {
     console.log("배송비 관련 비용을 읽음");
+    const readFeeEtc = async () => {
+      const feeEtc = await getFeeEtc();
+
+      setOriginPrices(feeEtc.data);
+      setFeeEtc(feeEtc.data);
+    };
+    readFeeEtc();
   }, []);
-  return <div>배송비 정보</div>;
+
+  const handleSubmit = () => {};
+
+  return (
+    <Form onSubmit={handleSubmit} className="d-flex justify-content-center">
+      <Card id="soapPriceCard">
+        <Card.Header className="text-center mb-2 h5">
+          배송비 관련 설정
+        </Card.Header>
+        <Card.Body className="d-flex align-items-center justify-content-center">
+          <div style={{ overflow: "auto" }}>
+            <Table id="soapPrice" className="my-0">
+              <tbody>
+              </tbody>
+            </Table>
+          </div>
+        </Card.Body>
+        <Card.Footer className="text-center">
+          <div className="d-flex justify-content-center mb-3 mt-3 char2button">
+            <Button
+              type="button"
+              // disabled={priceUnchanged()}
+              variant="success"
+              size="sm"
+              className="me-4"
+              // onClick={restorePrices}
+            >
+              {"복원"}
+            </Button>
+            <Button
+              type="submit"
+              // disabled={priceUnchanged()}
+              variant="primary"
+              size="sm"
+            >
+              {"저장"}
+            </Button>
+          </div>
+        </Card.Footer>
+      </Card>
+    </Form>
+  );
 };
 
 export default DeliveryFeeCard;
