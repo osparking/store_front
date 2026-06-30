4829c51b2cee5e1111a61d789170f7bc65456770
BumTiger <jbpark03@gmail.com>
2026-07-01 06:59:09 +0900

    고객상세정보 - 항목배열,map() 사용 테이블 폼화

--- src/component/modal/CustomerInfoModal.jsx
+++ src/component/modal/CustomerInfoModal.jsx
@@ -1,10 +1,19 @@
 import { useEffect } from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, Modal, Table } from "react-bootstrap";
+import "./CustomerInfoModal.css";
 
 const CustomerInfoModal = ({ show, onHide, customer }) => {
   useEffect(() => {
     console.dir("dir:", customer);
   }, [customer]);
+
+  const detailsItem = [
+    {
+      label: "성명",
+      name: "fullName",
+      value: customer.fullName,
+    },
+  ];
 
   return (
     <Modal show={show} onHide={onHide} dialogClassName="recipients-modal">
@@ -12,7 +21,37 @@ const CustomerInfoModal = ({ show, onHide, customer }) => {
         <Modal.Header closeButton>
           <Modal.Title>고객 상세 정보</Modal.Title>
         </Modal.Header>
-        <Modal.Body>성명: {customer?.fullName}</Modal.Body>
+        <Modal.Body>
+          <Form className="d-flex justify-content-center">
+            <Table id="customerDetails" className="my-0" style={{width: "75%"}}>
+              <tbody>
+                {detailsItem.map((item, index) => (
+                  <tr key={index}>
+                    <td
+                      md={4}
+                      className="text-end"
+                      style={{ minWidth: "145px", paddingRight: 0 }}
+                    >
+                      <Form.Label htmlFor={item.name}>{item.label} :</Form.Label>
+                    </td>
+                    <td
+                      md={7}
+                      colSpan={2}
+                      style={{ minWidth: "250px", paddingLeft: "4px" }}
+                    >
+                      <Form.Control
+                        plaintext
+                        readOnly
+                        defaultValue={item.value}
+                        className={"disabled-color ps-2"}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Form>
+        </Modal.Body>
         <Modal.Footer className="d-flex justify-content-center align-items-center">
           <div>
             <Button variant="secondary" onClick={onHide}>
