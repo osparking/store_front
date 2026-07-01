b3e8df2d7b58952a08fc5ef2f353dbc8699ed26d
BumTiger <jbpark03@gmail.com>
2026-07-01 10:37:42 +0900

    모달 바디 내용 (총 10 행) 전부 추가

--- src/component/modal/CustomerInfoModal.jsx
+++ src/component/modal/CustomerInfoModal.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Button, Form, Modal, Table } from "react-bootstrap";
 import "./CustomerInfoModal.css";
+import { insert2Hyphens } from "../util/utilities";
 
 const CustomerInfoModal = ({ show, onHide, customer }) => {
   useEffect(() => {
@@ -9,9 +10,54 @@ const CustomerInfoModal = ({ show, onHide, customer }) => {
 
   const detailsItem = [
     {
+      label: "아이디",
+      name: "id",
+      value: customer.id,
+    },
+    {
       label: "성명",
       name: "fullName",
       value: customer.fullName,
+    },
+    {
+      label: "휴대폰",
+      name: "mbPhone",
+      value: insert2Hyphens(customer.mbPhone),
+    },
+    {
+      label: "이메일",
+      name: "email",
+      value: customer.email,
+    },
+    {
+      label: "기본 수신처",
+      name: "recipientSet",
+      value: customer.recipientSet ? "지정됨" : "미지정",
+    },
+    {
+      label: "등록 형태",
+      name: "signUpMethod",
+      value: customer.signUpMethod,
+    },
+    {
+      label: "등록 일시",
+      name: "addDate",
+      value: customer.addDate,
+    },
+    {
+      label: "유저 구분",
+      name: "userType",
+      value: customer.userType,
+    },
+    {
+      label: "로그인",
+      name: "enabled",
+      value: customer.enabled ? "가능" : "불가능",
+    },
+    {
+      label: "구글 이중 인증(2FA)",
+      name: "twoFaEnabled",
+      value: customer.twoFaEnabled ? "활성화됨" : "비활성임",
     },
   ];
 
