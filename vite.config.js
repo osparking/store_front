04570c0bb0d3db0bc3f7093ffc6badceb00a4859
BumTiger <jbpark03@gmail.com>
2026-07-01 18:48:42 +0900

    드래그가능 모달 도서관 동작 오류 해소 구성 지정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // 브라우저에서 process.env를 참조할 수 있도록 빈 객체로 정의
+    "process.env": {},
+  },
+});
