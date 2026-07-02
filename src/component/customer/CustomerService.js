3dd7d03ca7fe53a34f3d3811b8b27544ff56a9f1
BumTiger <jbpark03@gmail.com>
2026-07-02 14:32:30 +0900

    고객 레코드 페이지 채취 API 함수

--- src/component/customer/CustomerService.js
+++ src/component/customer/CustomerService.js
@@ -1,5 +1,17 @@
 import { callWithToken } from "../util/api";
 
+export async function getCustomerPage(page, size) {
+  const urlPrefix = "/admin/get_customer_page?page=";
+
+  try {
+    const url = `${urlPrefix}${page}&size=${size}`;
+    const result = await callWithToken("get", url);
+    return result.data.data;
+  } catch (err) {
+    throw err;
+  }
+}
+
 export async function getCustomerList() {
   try {
     const url = "/admin/customer/get_all";
