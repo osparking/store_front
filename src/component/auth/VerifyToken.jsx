0162eaad84efc2536135973be3da75db870a081b
BumTiger <jbpark03@gmail.com>
2026-07-10 10:44:33 +0900

    비밀번호 리셋토큰 검사부터 리셋 실행 요청까지

--- src/component/auth/VerifyToken.jsx
+++ src/component/auth/VerifyToken.jsx
@@ -1,50 +1,42 @@
 import React, { useEffect, useState } from "react";
-import ProcessSpinner from "../common/ProcessSpinner";
-import { verify_token } from "./AuthService";
-import { storeJWT } from "../util/utilities";
-import PasswordResetModal from "../modal/PasswordResetModal";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import ProcessSpinner from "../common/ProcessSpinner";
+import PasswordResetModal from "../modal/PasswordResetModal";
+import { actResetPassword } from "../user/UserService";
+import { verify_token } from "./AuthService";
 
 const VerifyToken = () => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [verifyMsg, setVerifyMsg] = useState("토큰 검증 중입니다...");
-  const [alertType, setAlertType] = useState("alert-info");
   const [tokenExpired, setTokenExpired] = useState(false);
   const [email, setEmail] = useState("");
-  const [switchLabel, setSwitchLabel] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const [token, setToken] = useState("");
 
   const callVerifyToken = async (token) => {
     setIsProcessing(true);
     try {
       const response = await verify_token(token);
-      setSwitchLabel(response.message);
 
-      const jwtToken = response.data.token;
-      storeJWT(jwtToken, false);
+      setShowPasswordModal(true);
     } catch (error) {
       if (error.response) {
         const message = error.response.data.message;
         setEmail(error.response.data.data);
-        setAlertType("alert-danger");
 
         if (message === "만료된 토큰") {
-          setErrorMsg("비밀번호 리셋 토큰이 만료되었습니다.");
-          setAlertType("alert-warning");
+          setErrorMsg("만료된 비밀번호 리셋 토큰입니다.");
           setTokenExpired(true);
         } else if (message === "폐기된 토큰") {
-          setErrorMsg("이미 사용된 비밀번호 리셋 토큰입니다.");
-          setAlertType("alert-warning");
+          setErrorMsg("폐기된 비밀번호 리셋 토큰입니다.");
         } else if (message === "잘못된 토큰") {
-          setErrorMsg("비밀번호 리셋 토큰 오류입니다.");
-          setAlertType("alert-warning");
+          setErrorMsg("잘못된 비밀번호 리셋 토큰입니다.");
         } else {
           setErrorMsg(message);
         }
       } else {
-        // setErrorMsg("서버 연결 오류가 발생하였습니다.");
-        setErrorMsg(JSON.stringify(error));
+        setErrorMsg("서버 연결 네트워크 오류입니다.");
       }
     } finally {
       setIsProcessing(false);
@@ -55,7 +47,13 @@ const VerifyToken = () => {
 
   useEffect(() => {
     if (errorMsg) {
-      navigate("/login", { state: { prevError: errorMsg } });
+      navigate("/login", {
+        state: {
+          success: false,
+          message: errorMsg,
+          id: Date.now(),
+        },
+      });
     }
   }, [errorMsg]);
 
@@ -66,53 +64,61 @@ const VerifyToken = () => {
     const token = queryParams.get("token");
 
     if (token) {
+      setToken(token);
       if (!tokenVerifinRequested.current) {
         tokenVerifinRequested.current = true;
         callVerifyToken(token);
       }
     } else if (!token) {
-      setVerifyMsg("토큰이 제출되지 않았습니다.");
-      setAlertType("alert-danger");
+      setErrorMsg("토큰이 제출되지 않았습니다.");
     }
   }, []);
 
-  const [showPasswordModal, setShowPasswordModal] = useState(true);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
   const moveToLoginPage = () => {
     setShowPasswordModal(false);
-    window.location.href = "/login";
   };
   const [pwds, setPwds] = useState({
     newPwd: "",
     cnfPwd: "",
   });
 
-  const handleSubmit = () => {
-    let resetSuccess = true;
+  const handleSubmit = async () => {
+    const { newPwd, cnfPwd } = pwds;
+    let prevState = null;
 
-    // 후단에 비밀번호 갱신을 요구한다.
+    try {
+      // 후단에 비밀번호 갱신을 요구한다.
+      const result = await actResetPassword(newPwd, cnfPwd, token);
 
-    navigate("/login", {
-      state: resetSuccess
-        ? { prevSuccess: "비밀번호가 재 설정되었습니다." }
-        : { prevError: "비밀번호 재 설정 오류입니다." },
-    });
+      prevState = { success: true, message: result.message, id: Date.now() };
+    } catch (error) {
+      prevState = {
+        success: false,
+        message: error.response.data.message,
+        id: Date.now(),
+      };
+    } finally {
+      navigate("/login", {
+        state: prevState,
+      });
+    }
   };
 
   return (
     <>
       <PasswordResetModal
-        show={showPasswordModal && switchLabel !== ""}
+        show={showPasswordModal}
         closer={() => moveToLoginPage()}
-        switchLabel={switchLabel}
         doSubmit={handleSubmit}
         pwds={pwds}
         setPwds={setPwds}
       />
       <div
         className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh" }}
+        style={{ height: "100vh", color: "purple" }}
       >
-        {isProcessing && <ProcessSpinner message="토큰 검증" />}
+        {isProcessing && <ProcessSpinner message="비밀번호 토큰 검사" />}
       </div>
     </>
   );
