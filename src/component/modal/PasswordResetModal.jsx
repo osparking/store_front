545937bd6d92286b395ec8532ccbcb327ede29f3
BumTiger <jbpark03@gmail.com>
2026-07-11 09:02:03 +0900

    비밀번호 규칙 특수문자 목록- 자간 1px

--- src/component/modal/PasswordResetModal.jsx
+++ src/component/modal/PasswordResetModal.jsx
@@ -143,7 +143,10 @@ const PasswordResetModal = ({ show, closer, doSubmit, pwds, setPwds }) => {
           <li id="pwdRuleTopRow">허용 문자 유형: 영대, 영소, 숫자, 특수</li>
           <ul>
             <li className="no-bullet">
-              [특수] ! @ # $ % ^ & * ( ) - _ = +
+              (특수)
+              <span style={{letterSpacing: "1px"}}>{" "}
+                ! @ # $ % ^ & * ( ) - _ = +
+              </span>
             </li>
           </ul>
           <li id="midBreak">문자 유형별 한 자 이상 사용</li>
