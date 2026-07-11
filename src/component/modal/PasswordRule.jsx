b4a6e26013260936dfeca8c1574515befff38709
BumTiger <jbpark03@gmail.com>
2026-07-11 19:50:12 +0900

    비밀번호 규칙 순서 및 내용 다소 변경

--- src/component/modal/PasswordRule.jsx
+++ src/component/modal/PasswordRule.jsx
@@ -5,6 +5,8 @@ import { Button } from "react-bootstrap";
 const PasswordRule = () => {
   return (
     <ul className="password-rules">
+      <li>허용 유형 문자만으로 길이 9 이상</li>
+      <li>문자 유형별 한 자 이상 사용</li>
       <li>허용 문자 유형: 영대, 영소, 숫자, 특수</li>
       <ul className="special-chars">
         <li>
@@ -15,8 +17,6 @@ const PasswordRule = () => {
           </span>
         </li>
       </ul>
-      <li>문자 유형별 한 자 이상 사용</li>
-      <li>허용 문자만으로 길이 9 자 이상</li>
     </ul>
   );
 };
