440a03c71952b7e44098142719901ce780696e19
BumTiger <jbpark03@gmail.com>
2026-07-11 19:16:51 +0900

    비밀번호 규칙 팝업에 맞춰 성분 수정

--- src/component/modal/PasswordRule.jsx
+++ src/component/modal/PasswordRule.jsx
@@ -4,20 +4,20 @@ import { Button } from "react-bootstrap";
 
 const PasswordRule = () => {
   return (
-    <div>
-      <li id="pwdRuleTopRow">허용 문자 유형: 영대, 영소, 숫자, 특수</li>
-      <ul>
-        <li className="no-bullet">
+    <ul className="password-rules">
+      <li>허용 문자 유형: 영대, 영소, 숫자, 특수</li>
+      <ul className="special-chars">
+        <li>
           (특수)
-          <span style={{ letterSpacing: "1px" }}>
+          <span className="special-char-list">
             {" "}
             ! @ # $ % ^ & * ( ) - _ = +
           </span>
         </li>
       </ul>
-      <li id="midBreak">문자 유형별 한 자 이상 사용</li>
+      <li>문자 유형별 한 자 이상 사용</li>
       <li>허용 문자만으로 길이 9 자 이상</li>
-    </div>
+    </ul>
   );
 };
 
