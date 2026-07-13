1b58470f4585504961ef51285993218bbd9e976f
BumTiger <jbpark03@gmail.com>
2026-07-14 08:38:30 +0900

    api 함수 차원, JWT 만료 때 재 로그인 요구

--- src/component/util/api.js
+++ src/component/util/api.js
@@ -1,5 +1,6 @@
 import axios, { HttpStatusCode } from "axios";
 import toast from "react-hot-toast";
+import { logoutUser } from "../auth/AuthService";
 import { getStorageToken } from "./utilities";
 
 const prefix = "http://localhost:9193/api/s1";
@@ -55,6 +56,12 @@ export async function callWithToken(method, urlSuffix, data = null) {
     ) {
       toast.error(err?.response?.data.message);
       return null;
+    } else if (
+      err.response.status === HttpStatusCode.InternalServerError &&
+      err.response.data.message.includes("JWT expired")
+    ) {
+      logoutUser({path: "/login", message: "인증이 만료되었습니다."});
+      throw err;
     } else {
       throw err;
     }
