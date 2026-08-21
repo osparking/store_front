270e44f11c18de3920f3bdf437a7472afa544ee6
BumTiger <jbpark03@gmail.com>
2026-08-21 20:39:20 +0900

    장바구니 담은 결과 모달 갱신 - 두 버튼 푸터로...

--- src/component/buy/CartPutModal.jsx
+++ src/component/buy/CartPutModal.jsx
@@ -16,21 +16,23 @@ const CartPutModal = ({ show, closer, getResultString = [] }) => {
       </Modal.Header>
       <Modal.Body>
         <h5>변경 내용 요약</h5>
-        <ul>
+        <ul style={{ margin: "0 0 0 20px" }}>
           {getResultString.length > 0 ? (
             getResultString.map((item, index) => <li key={index}>{item}</li>)
           ) : (
             <li>장바구니에 담긴 상품이 없습니다</li>
           )}
         </ul>
+      </Modal.Body>
+      <Modal.Footer>
         <div
-          style={{ display: "flex", gap: "20px" }}
-          className="justify-content-center"
+          style={{ width: "100%" }}
+          className="char4button d-flex justify-content-center gap-3"
         >
           <Button
             variant="info"
             size="sm"
-            className="pt-2 pb-2 order-button-width"
+            className="order-button-width"
             onClick={showShoppingCart}
           >
             바구니 보기
@@ -38,15 +40,14 @@ const CartPutModal = ({ show, closer, getResultString = [] }) => {
           <Button
             variant="success"
             size="sm"
-            className="pt-2 pb-2 order-button-width"
+            className="order-button-width"
             onClick={closer}
             style={{ minWidth: "95px" }}
           >
             비누 주문
           </Button>
         </div>
-      </Modal.Body>
-      <Modal.Footer></Modal.Footer>
+      </Modal.Footer>
     </Modal>
   );
 };
