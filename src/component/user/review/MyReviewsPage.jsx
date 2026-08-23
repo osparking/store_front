02253f75d6d0a3e95d00fa2a7890a446d9fdd8f6
BumTiger <jbpark03@gmail.com>
2026-08-23 17:06:59 +0900

    리뷰모달을 내 리뷰테이블로 하향 배치

--- src/component/user/review/MyReviewsPage.jsx
+++ src/component/user/review/MyReviewsPage.jsx
@@ -2,15 +2,10 @@ import { useContext, useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "../../../App.css";
-import {
-  fetchReview,
-  getReviewPage,
-  patchOrderReview,
-} from "../../buy/orderService";
+import { getReviewPage } from "../../buy/orderService";
 import Paginator from "../../common/Paginator";
 import "../../home/home.css";
 import "../../pay_toss/MyOrdersPage.css";
-import ReviewModal from "../../review/ReviewModal";
 import { getRecordRange } from "../../util/utilities";
 import OrderDetail from "../../worker/order/OrderDetail";
 import { ReviewsContext } from "../UserDashboard";
@@ -52,34 +47,8 @@ const MyReviewsPage = () => {
     loadReviewPage();
   }, [currentPage]);
 
-  const navigate = useNavigate();
-
-  const goHome = () => {
-    navigate("/");
-  };
-
-  const [showReviewModal, setShowReviewModal] = useState(false);
-  const [review, setReview] = useState({});
-
-  const manageReview = async (review) => {
-    const reviewInfo = await fetchReview(review.id);
-    setReview({ ...reviewInfo, id: review.id, reviewTime: review.reviewTime });
-    setShowReviewModal(true);
-  };
-
-  const saveReview = async (reviewData) => {
-    setShowReviewModal(false);
-    await patchOrderReview(reviewData);
-    loadReviewPage();
-  };
-
   const [showDetail, setShowDetail] = useState(false);
   const [detailId, setDetailId] = useState(0);
-
-  const closeMyReviewModal = (reloadReviews) => {
-    reloadReviews && loadReviewPage();
-    setShowReviewModal(false);
-  };
 
   return (
     <>
@@ -90,51 +59,36 @@ const MyReviewsPage = () => {
           isHouse={false}
         />
       ) : (
-        <>
-          <ReviewModal
-            show={showReviewModal}
-            handleClose={closeMyReviewModal}
-            title={"후기 관리"}
-            review={review}
-            saveReview={saveReview}
-            editable={true}
-          />
-          <div
-            className="justify-content-center align-items-center my-card-container"
-            style={{ display: "flex", width: "100%" }}
-          >
-            <Card className="tableCard">
-              <Card.Body style={{ width: "100%" }}>
-                <h2 className="mb-1 ps-0">
-                  <strong>나의 후기 목록</strong>
-                </h2>
-                <p className="text-center text-muted mb-2">
-                  {getRecordRange(
-                    reviewPage,
-                    indexOfFirst,
-                    idxLastPlus1,
-                    "후기",
-                  )}
-                </p>
-                <div
-                  style={{ overflow: "auto" }}
-                  className="align-items-center justify-content-center"
-                >
-                  {MyReviewsTable(reviews, manageReview)}
-                </div>
-                {searchResult && reviewPage && (
-                  <Paginator
-                    pageSize={pageSize}
-                    totalItems={reviewPage.totalElements}
-                    totalPages={totalPages}
-                    currPage={currentPage}
-                    setCurrPage={(pageNo) => setCurrentPage(pageNo)}
-                  />
-                )}
-              </Card.Body>
-            </Card>
-          </div>
-        </>
+        <div
+          className="justify-content-center align-items-center my-card-container"
+          style={{ display: "flex", width: "100%" }}
+        >
+          <Card className="tableCard">
+            <Card.Body style={{ width: "100%" }}>
+              <h2 className="mb-1 ps-0">
+                <strong>나의 후기 목록</strong>
+              </h2>
+              <p className="text-center text-muted mb-2">
+                {getRecordRange(reviewPage, indexOfFirst, idxLastPlus1, "후기")}
+              </p>
+              <div
+                style={{ overflow: "auto" }}
+                className="align-items-center justify-content-center"
+              >
+                {MyReviewsTable(reviews)}
+              </div>
+              {searchResult && reviewPage && (
+                <Paginator
+                  pageSize={pageSize}
+                  totalItems={reviewPage.totalElements}
+                  totalPages={totalPages}
+                  currPage={currentPage}
+                  setCurrPage={(pageNo) => setCurrentPage(pageNo)}
+                />
+              )}
+            </Card.Body>
+          </Card>
+        </div>
       )}
     </>
   );
