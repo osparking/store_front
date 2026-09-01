1be0d222ecd0f4d8f24f0e515de656ffe1a2774a
BumTiger <jbpark03@gmail.com>
2026-09-01 18:55:03 +0900

    검색엔진 노출도 향상을 위한 수정 - 홈

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -71,89 +71,97 @@ const Home = () => {
   };
 
   return (
-    <Container fluid className="home-container mb-2">
-      <EducationalModal
-        show={showEduModal}
-        hours={hide_hours}
-        handleClose={closeEduModal}
-        setHideCheckBox={setHideCheckBox}
+    <>
+      <title>범이비누 - 수제비누</title>
+      <meta
+        name="description"
+        content="올리브오일 엑스트라버진 수제비누 범이비누(BumSoap) 홈페이지입니다."
       />
-      <Row className="justify-content-center homeRow">
-        <Col md={6} className="homeCol">
-          <Card>
-            <Card.Img
-              variant="top"
-              src={olive_label}
-              alt="엑스트라 버진 올리브 오일"
-              className="hero-image mb-3"
-            />
-            <Card.Title className="mt-2">소중한 피부의 친구~</Card.Title>
-            <Card.Body className="pt-0">
-              <Card.Text>
-                제가 어떤 수제비누를 관광지 해변 휴게실 근처 좌판대에서 만난
-                것은 2019년 봄이었을 겁니다. 평소 피부 가려움증으로 매해 가을의
-                시작과 함께 저는 이른바 피부 트러블로 이듬해 우수 경칩이 될
-                때까지 피부에 거의 온 신경을 쓰면서 지내야 했기 때문에
-                수제비누가 제게 안겨준 평온함은 정말 돈으로 가치를 따지기 어려울
-                정도였습니다.
+      <Container fluid className="home-container mb-2">
+        <EducationalModal
+          show={showEduModal}
+          hours={hide_hours}
+          handleClose={closeEduModal}
+          setHideCheckBox={setHideCheckBox}
+        />
+        <Row className="justify-content-center homeRow">
+          <Col md={6} className="homeCol">
+            <Card>
+              <Card.Img
+                variant="top"
+                src={olive_label}
+                alt="엑스트라 버진 올리브 오일"
+                className="hero-image mb-3"
+              />
+              <Card.Title className="mt-2">소중한 피부의 친구~</Card.Title>
+              <Card.Body className="pt-0">
+                <Card.Text>
+                  제가 어떤 수제비누를 관광지 해변 휴게실 근처 좌판대에서 만난
+                  것은 2019년 봄이었을 겁니다. 평소 피부 가려움증으로 매해
+                  가을의 시작과 함께 저는 이른바 피부 트러블로 이듬해 우수
+                  경칩이 될 때까지 피부에 거의 온 신경을 쓰면서 지내야 했기
+                  때문에 수제비누가 제게 안겨준 평온함은 정말 돈으로 가치를
+                  따지기 어려울 정도였습니다.
+                </Card.Text>
+                <Card.Text>
+                  아무리 그래도 100 그램 비누 한장이 사천원에서 하루 아침에
+                  6천원이 되고 또 8천원이 되자 저는 직접 만들어서 사용해 보자고
+                  생각했습니다. 유튜브를 검색하던 중 저는 어떤 여성 유튜버의
+                  올리브오일 엑스트라 버진 수제비누 제조 영상을 보게 되었습니다.
+                  저는 여러가지 도구와 재료를 인터넷 몰에서 구입하여 일단 비누
+                  열장을 만들어 사용해 보았고, 그 효과는 '19년 봄에 만났던 그
+                  수제비누와 마찬가지로 제 허벅지 피부 가려움증을 거의 해소시켜
+                  주었습니다.
+                </Card.Text>
+                <div className="text-center mt-1 button-div">
+                  <Button variant="outline-info" onClick={moveToSoapIntro}>
+                    범이비누 소개 보기
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={6} className="homeCol">
+            <Card id="homeRightCard">
+              <Card.Img
+                variant="top"
+                src={soap_rack}
+                alt="범이비누 정보"
+                className="hero-image"
+              />
+              <Card.Text id="homeImageLabel" className="bumsoap-color-dark">
+                숙성 중인 범이비누
               </Card.Text>
-              <Card.Text>
-                아무리 그래도 100 그램 비누 한장이 사천원에서 하루 아침에
-                6천원이 되고 또 8천원이 되자 저는 직접 만들어서 사용해 보자고
-                생각했습니다. 유튜브를 검색하던 중 저는 어떤 여성 유튜버의
-                올리브오일 엑스트라 버진 수제비누 제조 영상을 보게 되었습니다.
-                저는 여러가지 도구와 재료를 인터넷 몰에서 구입하여 일단 비누
-                열장을 만들어 사용해 보았고, 그 효과는 '19년 봄에 만났던 그
-                수제비누와 마찬가지로 제 허벅지 피부 가려움증을 거의 해소시켜
-                주었습니다.
-              </Card.Text>
-              <div className="text-center mt-1 button-div">
-                <Button variant="outline-info" onClick={moveToSoapIntro}>
-                  범이비누 소개 보기
-                </Button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={6} className="homeCol">
-          <Card id="homeRightCard">
-            <Card.Img
-              variant="top"
-              src={soap_rack}
-              alt="범이비누 정보"
-              className="hero-image"
-            />
-            <Card.Text id="homeImageLabel" className="bumsoap-color-dark">
-              숙성 중인 범이비누
-            </Card.Text>
-            <Card.Title>관련 정보 목록</Card.Title>
-            <Card.Body className="mt-0 pt-0">
-              <ListGroup className="services-list">
-                <ListGroup.Item action onClick={gotoIngredients}>
-                  비누 재료
-                </ListGroup.Item>
-                <ListGroup.Item action onClick={gotoSoapImages}>
-                  비누 영상
-                </ListGroup.Item>
-                <ListGroup.Item action onClick={gotoReviews}>
-                  구매 후기
-                </ListGroup.Item>
-              </ListGroup>
-              <Card.Text className="mt-3">
-                고객님들의 성원과 참여도에 따라 재료 대량 구매, 생산 작업의 부분
-                자동화 등을 통하여 비누 가격이 내려갈 수 있으며, 에센셜 향오일을
-                사용하는 등의 품질 고급화 및 다양화가 가능할 수 있습니다.
-              </Card.Text>
-              <div className="text-center mt-1 button-div">
-                <Button variant="outline-info" onClick={moveToBuySoap}>
-                  범이비누 사러가기
-                </Button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+              <Card.Title>관련 정보 목록</Card.Title>
+              <Card.Body className="mt-0 pt-0">
+                <ListGroup className="services-list">
+                  <ListGroup.Item action onClick={gotoIngredients}>
+                    비누 재료
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={gotoSoapImages}>
+                    비누 영상
+                  </ListGroup.Item>
+                  <ListGroup.Item action onClick={gotoReviews}>
+                    구매 후기
+                  </ListGroup.Item>
+                </ListGroup>
+                <Card.Text className="mt-3">
+                  고객님들의 성원과 참여도에 따라 재료 대량 구매, 생산 작업의
+                  부분 자동화 등을 통하여 비누 가격이 내려갈 수 있으며, 에센셜
+                  향오일을 사용하는 등의 품질 고급화 및 다양화가 가능할 수
+                  있습니다.
+                </Card.Text>
+                <div className="text-center mt-1 button-div">
+                  <Button variant="outline-info" onClick={moveToBuySoap}>
+                    범이비누 사러가기
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
